1c4c05dacf6af55d5b07bb7b21f2109b5e480cb0
Joao Victor <jov9051@outlook.com>
2022-01-29 15:34:09 -0300

    router declarado dentro de index.js e middleware sendo usado para rotas /api

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ require('dotenv').config();
 
 let express = require('express');
 
+let router = require('./routes/router');
+
 let app = express();
+
+app.use('/api', router);
 
 app.listen(process.env.NODE_ENV_SERVER_PORT, () => {
     console.log(`Server running on port: ${process.env.NODE_ENV_SERVER_PORT}`);
