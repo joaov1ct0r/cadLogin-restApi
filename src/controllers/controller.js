78e6ce999f458e96189cb31655eec38552b2a3d1
Joao Victor <jov9051@outlook.com>
2022-01-29 15:59:41 -0300

    funçao de login criada em user no arquivo controller.js

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,3 +1,11 @@
 let user = {
-    database: []
+    database: [],
+
+    login: function (email, senha, callback) {
+        if (email === this.database.email && senha === this.database.senha) {
+            callback('Login realizado com sucesso');
+        } else {
+            callback('Falha na autenticação');
+        }
+    }
 };
