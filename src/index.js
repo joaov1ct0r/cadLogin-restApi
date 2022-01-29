5eb1cecdd09b5b49ed28315504fec258919786da
Joao Victor <jov9051@outlook.com>
2022-01-28 21:39:59 -0300

    .env movido para diretorio jwt-restApi

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 
 let express = require('express');
 
 let app = express();
 
-app.listen(process.env.SERVER_PORT, () => {
+app.listen(process.env.PORT, () => {
     console.log('Server running');
 });
