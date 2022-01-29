e91d5ce39f573915be90758103804a9159587a3b
Joao Victor <jov9051@outlook.com>
2022-01-28 21:59:02 -0300

    dotenv MOVIDO PARA A PASTA src

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 require('dotenv').config();
 
 let express = require('express');
-
-let app = express();
-
-app.listen(process.env.NODE_ENV_SERVER_PORT, () => {
-    console.log(`Server running on port: ${process.env.NODE_ENV_SERVER_PORT}`);
-});
