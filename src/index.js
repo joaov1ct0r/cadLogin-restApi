100e25a207cc3a117d549fdc0865d5b4aea0ca4e
Joao Victor <jov9051@outlook.com>
2022-01-28 22:27:30 -0300

    rota /login criada utilizando o metodo post

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ let bodyParser = require('body-parser');
 
 let app = express();
 
+app.post('/login', bodyParser.json(), (req, res) => {
+    res.send('Rota funcionando');
+});
+
 app.listen(process.env.NODE_ENV_SERVER_PORT, () => {
     console.log(`Server running on port: ${process.env.NODE_ENV_SERVER_PORT}`);
 });
