9922e94efce6f14bcbeaad5f529a38dbbba209e3
Joao Victor <jov9051@outlook.com>
2022-01-28 21:59:41 -0300

    variavel app criada no arquivo index.js executando o express, app escutando NODE_ENV_SERVER_PORT

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 require('dotenv').config();
 
 let express = require('express');
+
+let app = express();
+
+app.listen(process.env.NODE_ENV_SERVER_PORT, () => {
+    console.log(`Server running on port: ${process.env.NODE_ENV_SERVER_PORT}`);
+});
