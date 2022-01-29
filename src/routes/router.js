b48074df11625cc1e8eba638b77c6203818587b2
Joao Victor <jov9051@outlook.com>
2022-01-29 15:31:51 -0300

    rota /login movida para router.js

--- src/routes/router.js
+++ src/routes/router.js
@@ -5,3 +5,13 @@ let router = express.Router();
 let bodyParser = require('body-parser');
 
 let user = [];
+
+router.post('/login', bodyParser.json(), (req, res) => {
+    let { email } = req.body;
+
+    let { senha } = req.body;
+
+    if (email === user[0].email && senha === user[0].senha) {
+        res.send('Rota funcionando');
+    }
+});
