3acb58473afd5f32cf4151e707eef1414f4daa98
Joao Victor <jov9051@outlook.com>
2022-01-30 20:35:10 -0300

    userSchema criado e sendo exportado em User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,1 +1,10 @@
 const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    email: { type: String, required: true, minlength: 10, maxlength: 100 },
+    nome: { type: String, required: true, minlength: 10, maxlength: 100 },
+    idade: { type: Number, required: true, minlength: 1, maxlength: 2 },
+    senha: { type: String, required: true, minlength: 8, maxlength: 100 }
+});
+
+module.exports = mongoose.model('User', userSchema);
