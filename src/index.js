15f0858440f4a89d0922c8e8f125458d28205b13
Joao Victor <jov9051@outlook.com>
2022-01-30 19:47:30 -0300

    conexao mysql funcionando

--- src/index.js
+++ src/index.js
@@ -8,6 +8,22 @@ let router = require('./routes/router');
 
 let app = express();
 
+mysql.createConnection(
+    {
+        host: process.env.NODE_ENV_DB_HOST,
+        user: process.env.NODE_ENV_DB_USER,
+        password: process.env.NODE_ENV_DB_PASSWORD,
+        database: process.env.NODE_ENV_DB_DB
+    },
+    error => {
+        if (error) {
+            throw error;
+        } else {
+            console.log('MySQL Conectado');
+        }
+    }
+);
+
 app.use('/api', router);
 
 app.listen(process.env.NODE_ENV_SERVER_PORT, () => {
