9dbc7acff23554296d83109543a69b69be286aac
Joao Victor <jov9051@outlook.com>
2022-02-01 16:29:32 -0300

    rota /login utilizando metodo authController e depois controller.login

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -6,7 +6,7 @@ let authController = require('../controllers/authController');
 
 let controller = require('../controllers/userController');
 
-router.post('/login', controller.login);
+router.post('/login', authController, controller.login);
 
 router.post('/register', controller.register);
 
