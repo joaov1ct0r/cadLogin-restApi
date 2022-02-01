174aac2d980a3a08655748264650ff48edb0918b
Joao Victor <jov9051@outlook.com>
2022-02-01 16:28:55 -0300

    authController declarado em userRouter.js

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -2,6 +2,8 @@ let express = require('express');
 
 let router = express.Router();
 
+let authController = require('../controllers/authController');
+
 let controller = require('../controllers/userController');
 
 router.post('/login', controller.login);
