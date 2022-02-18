c6d64a0a2732f26e47dc02d6d7443ea3483c39ce
Joao Victor <jov9051@outlook.com>
2022-02-18 20:24:08 -0300

    joi schema criado dentro da var schema em registerValidate em validateData.js

--- src/controllers/validateData.js
+++ src/controllers/validateData.js
@@ -1,5 +1,10 @@
 let Joi = require('@hapi/joi');
 
 const registerValidate = data => {
-    const schema = Joi.object({});
+    const schema = Joi.object({
+        email: Joi.string().required().min(10).max(100),
+        nome: Joi.string().required().min(10).max(100),
+        idade: Joi.date().required(),
+        senha: Joi.string().required().min(8).max(100)
+    });
 };
