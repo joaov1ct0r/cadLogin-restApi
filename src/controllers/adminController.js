7d42989b1b82d0951e9353788aacf77d2444e442
Joao Victor <jov9051@outlook.com>
2022-03-26 15:56:45 -0300

    metodo em adminController.js alterado para arrow function

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,11 +1,9 @@
-let admin = {
-    validateAdmin(req, res) {
-        if (req.user.admin) {
-            res.send('Dados do admin');
-        } else {
-            res.status(401).send('Acesso negado');
-        }
+const validateAdmin = (req, res) => {
+    if (req.user.admin) {
+        res.send('Dados do admin');
+    } else {
+        res.status(401).send('Acesso negado');
     }
 };
 
-module.exports = admin;
+export default validateAdmin;
