798704ec8652bc310c30751b5ac0e627cb3aec3d
Joao Victor <jov9051@outlook.com>
2022-03-26 17:00:26 -0300

    dbConnection sendo exportada em db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,20 @@
 import mongoose from 'mongoose';
 
-const dbConnection = mongoose.connect(
-    process.env.NODE_ENV_DB_HOST,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    },
-    error => {
-        if (error) {
-            throw error;
-        } else {
-            console.log('Mongoose Conectado');
+const dbConnection = () => {
+    mongoose.connect(
+        process.env.NODE_ENV_DB_HOST,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        },
+        error => {
+            if (error) {
+                throw error;
+            } else {
+                console.log('Mongoose Conectado');
+            }
         }
-    }
-);
+    );
+};
 
 export default dbConnection;
