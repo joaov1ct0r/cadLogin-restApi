5cfdb3f678511d1d67ebf25ed108a0cd537dc804
Joao Victor <jov9051@outlook.com>
2022-03-26 16:28:33 -0300

    authController importado para adminRouter.js

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -1,9 +1,11 @@
 import validateAdmin from '../controllers/adminController.js';
 
+import authController from '../controllers/authController.js';
+
 import express from 'express';
 
 const adminRouter = express.Router();
 
-adminRouter.get('/', validateAdmin);
+adminRouter.get('/', authController, validateAdmin);
 
 export default adminRouter;
