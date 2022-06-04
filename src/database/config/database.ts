c36e0c8c51216b225f4ed6b66805831d97682625
Joao Victor <jov9051@outlook.com>
2022-06-04 12:10:51 -0300

    feat: database.ts tipado

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -1,1 +1,14 @@
 import sequelize from "sequelize";
+
+const DB: sequelize.Sequelize = new sequelize.Sequelize(
+  process.env.DB_DATABASE as string,
+  process.env.DB_USER as string,
+  process.env.DB_PASSWORD as string,
+  {
+    dialect: "mysql",
+    host: process.env.DB_HOST as string,
+    port: Number(process.env.SERVER_PORT),
+  }
+);
+
+export default DB;
