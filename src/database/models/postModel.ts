20e78bd9c471c4db2f6ca4fe4bbbaf4b19216e42
Joao Victor <jov9051@outlook.com>
2022-06-04 13:28:49 -0300

    feat: Post sendo exportado em postModel.js

--- src/database/models/postModel.ts
+++ src/database/models/postModel.ts
@@ -1,3 +1,31 @@
 import DB from "../config/database";
 
-import { ModelStatic, Model } from "sequelize";
+import { ModelStatic, Model, DataTypes } from "sequelize";
+
+const Post: ModelStatic<Model> = DB.define("posts", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    unique: true,
+    autoIncrement: true,
+  },
+  author: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  content: {
+    type: DataTypes.STRING(250),
+    allowNull: false,
+  },
+  likes: {
+    type: DataTypes.ARRAY(DataTypes.STRING(250)),
+    allowNull: false,
+  },
+  comments: {
+    type: DataTypes.ARRAY(DataTypes.STRING(250)),
+  },
+});
+
+Post.sync();
+
+export default Post;
