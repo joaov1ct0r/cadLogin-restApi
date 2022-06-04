063f0c6115e298982adfcf747ddd55f59df29744
Joao Victor <jov9051@outlook.com>
2022-06-04 12:11:26 -0300

    refactor: /models movido para /database

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,4 @@
-import User from '../models/User.js';
+import User from '../database/models/User.js';
 
 import { registerValidate, loginValidate } from './validateData.js';
 
@@ -7,56 +7,56 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 const login = async (req, res) => {
-    const { error } = loginValidate(req.body);
+  const { error } = loginValidate(req.body);
 
-    if (error) {
-        return res.status(400).send('Falha na autenticação');
-    }
-    let selectedUser = await User.findOne({ email: req.body.email });
+  if (error) {
+    return res.status(400).send('Falha na autenticação');
+  }
+  let selectedUser = await User.findOne({ email: req.body.email });
 
-    if (!selectedUser) return res.status(400).send('Falha na autenticação');
+  if (!selectedUser) return res.status(400).send('Falha na autenticação');
 
-    let passwordCompare = bcrypt.compareSync(
-        req.body.password,
-        selectedUser.password
-    );
+  let passwordCompare = bcrypt.compareSync(
+    req.body.password,
+    selectedUser.password
+  );
 
-    if (!passwordCompare) return res.status(400).send('Falha na autenticação');
+  if (!passwordCompare) return res.status(400).send('Falha na autenticação');
 
-    const token = jwt.sign(
-        { id: selectedUser.id, admin: selectedUser.admin },
-        process.env.NODE_ENV_TOKEN_SECRET
-    );
-    res.header('auth-token', token);
+  const token = jwt.sign(
+    { id: selectedUser.id, admin: selectedUser.admin },
+    process.env.NODE_ENV_TOKEN_SECRET
+  );
+  res.header('auth-token', token);
 
-    res.send('Login realizado com sucesso');
+  res.send('Login realizado com sucesso');
 };
 
 const register = async (req, res) => {
-    let { error } = registerValidate(req.body);
+  let { error } = registerValidate(req.body);
 
-    if (error) {
-        return res.status(400).send('Falha no cadastramento');
-    }
-    let selectedUser = await User.findOne({ email: req.body.email });
+  if (error) {
+    return res.status(400).send('Falha no cadastramento');
+  }
+  let selectedUser = await User.findOne({ email: req.body.email });
 
-    if (selectedUser) return res.status(400).send('Email ja cadastrado');
-    const user = new User({
-        email: req.body.email,
-        nome: req.body.nome,
-        idade: req.body.idade,
-        senha: bcrypt.hashSync(req.body.senha)
-    });
+  if (selectedUser) return res.status(400).send('Email ja cadastrado');
+  const user = new User({
+    email: req.body.email,
+    nome: req.body.nome,
+    idade: req.body.idade,
+    senha: bcrypt.hashSync(req.body.senha)
+  });
 
-    try {
-        const savedUser = await user.save();
+  try {
+    const savedUser = await user.save();
 
-        if (!savedUser) return res.status(400).send('Falha no cadastramento');
+    if (!savedUser) return res.status(400).send('Falha no cadastramento');
 
-        res.send(savedUser);
-    } catch (error) {
-        throw error;
-    }
+    res.send(savedUser);
+  } catch (error) {
+    throw error;
+  }
 };
 
 export { login, register };
