1dec5f56d58450f60423e9dcaacde0618120b56c
Joao Victor <jov9051@outlook.com>
2022-06-04 16:05:26 -0300

    refactor: authController.js movido para /middlewares

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -1,11 +1,11 @@
-import validateAdmin from '../controllers/adminController.js';
+import validateAdmin from "../controllers/adminController.js";
 
-import authController from '../controllers/authController.js';
+import authController from "../middlewares/authController.js";
 
-import express from 'express';
+import express from "express";
 
 const adminRouter = express.Router();
 
-adminRouter.get('/', authController, validateAdmin);
+adminRouter.get("/", authController, validateAdmin);
 
 export default adminRouter;
