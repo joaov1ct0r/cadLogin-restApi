78204491a1ee25046161e6b2c604c2552fe805a2
Joao Victor <jov9051@outlook.com>
2022-06-04 12:17:36 -0300

    feat: IUser sendo exportado de userInterface.ts

--- src/types/userInterface.ts
+++ src/types/userInterface.ts
@@ -1,1 +1,11 @@
 import { Model } from "sequelize";
+
+interface IUser extends Model {
+  email: string;
+  senha: string;
+  nome: string;
+  idade: string;
+  admin: boolean;
+}
+
+export default IUser;
