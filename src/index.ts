90ee0d11b0cad9a2a1de14624514988cdde6207a
Joao Victor <jov9051@outlook.com>
2022-06-04 15:37:02 -0300

    build: body-parser desinstalado

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,23 @@
-import 'dotenv/config';
+import "dotenv/config";
 
-import userRouter from './routes/userRouter.js';
+import userRouter from "./routes/userRouter.js";
 
-import adminRouter from './routes/adminRouter.js';
+import adminRouter from "./routes/adminRouter.js";
 
-import bodyParser from 'body-parser';
+import express from "express";
 
-import express from 'express';
+import dbConnection from "./config/db.js";
 
-import dbConnection from './config/db.js';
-
-let app = express();
+const app = express();
 
 dbConnection();
 
 app.use(bodyParser.json());
 
-app.use('/usuario', userRouter);
+app.use("/usuario", userRouter);
 
-app.use('/admin', adminRouter);
+app.use("/admin", adminRouter);
 
 app.listen(process.env.NODE_ENV_SERVER_PORT, () => {
-    console.log(`Server running on port: ${process.env.NODE_ENV_SERVER_PORT}`);
+  console.log(`Server running on port: ${process.env.NODE_ENV_SERVER_PORT}`);
 });
