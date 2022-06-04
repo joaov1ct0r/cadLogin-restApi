838a5bb83caf0af3086e3020bf51ed116e00ebd9
Joao Victor <jov9051@outlook.com>
2022-06-04 16:09:10 -0300

    refactor: auth.ts tipado

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,16 +1,21 @@
 import jwt from "jsonwebtoken";
 
 export default function (req, res, next) {
-  const token = req.header("auth-token");
+  const token = req.cookies.authentication.split(" ")[1];
 
-  if (!token) return res.status(401).send("Acesso negado");
+  if (token.length === 0) {
+    return res.status(400).json({ error: "Token não encontrado!" });
+  }
 
   try {
-    const userVerified = jwt.verify(token, process.env.NODE_ENV_TOKEN_SECRET);
+    const userVerified = jwt.verify(
+      token,
+      process.env.JWT_TOKEN_SECRET as string
+    );
 
     if (!userVerified) return res.status(401).send("Acesso negado");
     next();
-  } catch (error) {
-    throw error;
+  } catch (err: unknown) {
+    return res.status(500).json({ err });
   }
 }
