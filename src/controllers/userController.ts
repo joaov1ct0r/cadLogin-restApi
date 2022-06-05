bfaea5b6685f03d58e04531e63a4bb2848f011ea
Joao Victor <jov9051@outlook.com>
2022-06-05 19:44:00 -0300

    refactor: handleUserRegister em userController.ts tipado

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -11,13 +11,50 @@ import {
 
 import IReq from "../types/requestInterface";
 
-import { Response, NextFunction } from "express";
+import { Request, Response } from "express";
 
 import { Model } from "sequelize";
 
 import bcrypt from "bcryptjs";
 
 import jwt from "jsonwebtoken";
+import IUser from "../types/userInterface";
+
+const handleUserRegister = async (req: Request, res: Response) => {
+  const { error } = validateHandleUserRegister(req.body);
+
+  if (error) {
+    return res.status(400).json({ error });
+  }
+
+  const email: string = req.body.email;
+
+  const password: string = req.body.password;
+
+  const name: string = req.body.name;
+
+  const bornAt: string = req.body.bornAt;
+  try {
+    const isUserRegistered: IUser | null = await User.findOne({
+      where: { email },
+    });
+
+    if (isUserRegistered === null) {
+      return res.status(400).json({ error: "Usuario já cadastrado!" });
+    }
+
+    const newUser: IUser = await User.create({
+      email,
+      password: bcrypt.hashSync(password),
+      name,
+      bornAt,
+    });
+
+    return res.status(201).json({ newUser });
+  } catch (err: unknown) {
+    return res.status(500).json({ err });
+  }
+};
 
 const login = async (req, res) => {
   const { error } = loginValidate(req.body);
@@ -43,33 +80,6 @@ const login = async (req, res) => {
   res.header("auth-token", token);
 
   res.send("Login realizado com sucesso");
-};
-
-const register = async (req, res) => {
-  const { error } = registerValidate(req.body);
-
-  if (error) {
-    return res.status(400).send("Falha no cadastramento");
-  }
-  const selectedUser = await User.findOne({ email: req.body.email });
-
-  if (selectedUser) return res.status(400).send("Email ja cadastrado");
-  const user = new User({
-    email: req.body.email,
-    nome: req.body.nome,
-    idade: req.body.idade,
-    senha: bcrypt.hashSync(req.body.senha),
-  });
-
-  try {
-    const savedUser = await user.save();
-
-    if (!savedUser) return res.status(400).send("Falha no cadastramento");
-
-    res.send(savedUser);
-  } catch (error) {
-    throw error;
-  }
 };
 
 export { login, register };
