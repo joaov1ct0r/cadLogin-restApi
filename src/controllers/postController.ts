8387abfe10eb381d1dd5006863efbe3a694f3868
Joao Victor <jov9051@outlook.com>
2022-06-06 17:12:34 -0300

    feat: IReq importado para postController.ts

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -3,3 +3,5 @@ import User from "../database/models/userModel";
 import Post from "../database/models/postModel";
 
 import { Request, Response } from "express";
+
+import IReq from "../types/requestInterface";
