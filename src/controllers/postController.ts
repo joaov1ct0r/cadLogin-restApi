2d331b23a665715de95c209f2753efc6567042d9
Joao Victor <jov9051@outlook.com>
2022-06-06 17:22:49 -0300

    feat: handleNewPost exportado em postController.ts

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -16,3 +16,37 @@ import {
 import IUser from "../types/userInterface";
 
 import { Model } from "sequelize";
+
+const handleNewPost = async (req: IReq, res: Response) => {
+  const { error } = validateHandleNewPost(req.body);
+
+  if (error) {
+    return res.status(400).json({ error });
+  }
+
+  const id: string | undefined = req.userId;
+
+  const content: string = req.body.content;
+
+  try {
+    const user: IUser | null = await User.findOne({
+      where: { id },
+    });
+
+    if (user === null) {
+      return res.status(404).json({ error: "Usuario não encontrado!" });
+    }
+
+    const newPost: Model = await Post.create({
+      author: user.email,
+      content,
+      userId: user.id,
+    });
+
+    return res.status(201).json({ newPost });
+  } catch (err: unknown) {
+    return res.status(500).json({ err });
+  }
+};
+
+export { handleNewPost };
--- src/validators/validatePostData.ts
+++ src/validators/validatePostData.ts
@@ -2,7 +2,6 @@ import Joi from "@hapi/joi";
 
 const validateHandleNewPost = (data: Object): Joi.ValidationResult => {
   const schema: Joi.ObjectSchema<Object> = Joi.object({
-    author: Joi.string().required().min(10).max(100),
     content: Joi.string().required().min(1).max(250),
   });
 
