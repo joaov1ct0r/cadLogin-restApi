b7a12f16046333ed80b83ea17a20047222b3c829
Joao Victor <jov9051@outlook.com>
2022-06-06 12:22:55 -0300

    feat: validateHandleEditPost exportado em validatePostData.ts

--- src/validators/validatePostData.ts
+++ src/validators/validatePostData.ts
@@ -3,10 +3,18 @@ import Joi from "@hapi/joi";
 const validateHandleNewPost = (data: Object): Joi.ValidationResult => {
   const schema: Joi.ObjectSchema<Object> = Joi.object({
     author: Joi.string().required().min(10).max(100),
-    content: Joi.string().required().max(250),
+    content: Joi.string().required().min(1).max(250),
   });
 
   return schema.validate(data);
 };
 
-export { validateHandleNewPost };
+const validateHandleEditPost = (data: Object): Joi.ValidationResult => {
+  const schema: Joi.ObjectSchema<Object> = Joi.object({
+    content: Joi.string().required().min(1).max(250),
+  });
+
+  return schema.validate(data);
+};
+
+export { validateHandleNewPost, validateHandleEditPost };
