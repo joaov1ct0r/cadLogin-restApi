df8218a07c84cf836d46f9fd6983607035092edd
Joao Victor <jov9051@outlook.com>
2022-06-06 12:04:52 -0300

    feat: admin.ts tipado

--- src/middlewares/admin.ts
+++ src/middlewares/admin.ts
@@ -1,3 +1,17 @@
 import IReq from "../types/requestInterface";
 
 import { Response, NextFunction } from "express";
+
+export default function (req: IReq, res: Response, next: NextFunction) {
+  try {
+    const isUserAdmin: boolean = req.admin === true;
+
+    if (isUserAdmin === false) {
+      return res.status(401).json({ error: "Não autorizado!" });
+    }
+
+    next();
+  } catch (err: unknown) {
+    return res.status(500).json({ err });
+  }
+}
