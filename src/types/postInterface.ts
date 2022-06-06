4e0a97dbd53286b8903eff11757d13bd9417f1ff
Joao Victor <jov9051@outlook.com>
2022-06-06 17:34:40 -0300

    feat: IPost sendo exportado em postInterface.ts

--- src/types/postInterface.ts
+++ src/types/postInterface.ts
@@ -1,1 +1,11 @@
 import { Model } from "sequelize";
+
+interface IPost extends Model {
+  id: string;
+  author: string;
+  content: string;
+  likes?: string[];
+  comments: string[];
+}
+
+export default IPost;
