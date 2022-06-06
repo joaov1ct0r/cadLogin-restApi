28b73d820a3f531313afc1bd95a8bec028251c84
Joao Victor <jov9051@outlook.com>
2022-06-06 19:20:23 -0300

    feat: metodos de postController.ts importados para postRoutes.ts

--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -2,4 +2,12 @@ import express from "express";
 
 import auth from "../middlewares/auth";
 
+import {
+  handleAllPosts,
+  handleDeletePost,
+  handleEditPost,
+  handleNewPost,
+  handleOnePost,
+} from "../controllers/postController";
+
 const userRouter: express.Router = express.Router();
