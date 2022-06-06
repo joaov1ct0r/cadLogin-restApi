e303d388e4b27c6a2b8e1307cf7fb207a1691608
Joao Victor <jov9051@outlook.com>
2022-06-06 12:09:31 -0300

    feat: admin.ts tipado

--- src/middlewares/admin.ts
+++ src/middlewares/admin.ts
@@ -6,10 +6,18 @@ import IUser from "../types/userInterface";
 
 import User from "../database/models/userModel";
 
-export default function (req: IReq, res: Response, next: NextFunction) {
+export default async function (req: IReq, res: Response, next: NextFunction) {
   const id: string | undefined = req.userId;
+
   try {
-    const isUserAdmin: boolean = req.admin === true;
+    const isUserRegistered: IUser | null = await User.findOne({
+      where: { id },
+    });
+
+    if (isUserRegistered === null) {
+      return res.status(404).json({ error: "Usuario não encontrado!" });
+    }
+    const isUserAdmin: boolean = isUserRegistered.admin === true;
 
     if (isUserAdmin === false) {
       return res.status(401).json({ error: "Não autorizado!" });
