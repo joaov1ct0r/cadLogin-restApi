64058df515bc30518c5d62bdcf4871014f351d51
Joao Victor <jov9051@outlook.com>
2022-06-07 13:21:02 -0300

    feat: metodos de adminController.ts importados para adminRoutes.ts

--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -4,6 +4,12 @@ import auth from "../middlewares/auth";
 
 import admin from "../middlewares/admin";
 
+import {
+  handleAdminDeletePost,
+  handleAdminDeleteUser,
+  handleAdminEditUser,
+} from "../controllers/adminController";
+
 const adminRouter: express.Router = express.Router();
 
 adminRouter.get("/", authController, validateAdmin);
