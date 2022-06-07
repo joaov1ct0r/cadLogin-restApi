d9a9e632766abc237975ea42d26a484183902dcd
Joao Victor <jov9051@outlook.com>
2022-06-07 12:41:55 -0300

    feat: validateHandleAdminEditUser tipado em validateAdminData.ts

--- src/validators/validateAdminData.ts
+++ src/validators/validateAdminData.ts
@@ -1,1 +1,12 @@
 import Joi from "@hapi/joi";
+
+const validateHandleAdminEditUser = (data: Object): Joi.ValidationResult => {
+  const schema: Joi.ObjectSchema<Object> = Joi.object({
+    email: Joi.string().required().min(10).max(100),
+    name: Joi.string().required().min(10).max(100),
+    bornAt: Joi.date().required(),
+    password: Joi.string().required().min(8).max(100),
+  });
+
+  return schema.validate(data);
+};
