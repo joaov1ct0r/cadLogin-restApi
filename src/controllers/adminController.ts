3ac68e9e9c74f7e92984534fe0e0ed1783469b45
Joao Victor <jov9051@outlook.com>
2022-06-07 12:49:54 -0300

    feat: res importado para adminController.ts

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -3,3 +3,5 @@ import User from "../database/models/userModel";
 import Post from "../database/models/postModel";
 
 import IReq from "../types/requestInterface";
+
+import { Response } from "express";
