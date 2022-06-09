182a7db0cbb7bcd6df4afb8340ac6876424fa616
Joao Victor <jov9051@outlook.com>
2022-06-09 16:56:29 -0300

    refactor: port retirado de database.ts

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -3,7 +3,6 @@ import sequelize from "sequelize";
 const DB: sequelize.Sequelize = new sequelize.Sequelize({
   dialect: "postgres",
   host: process.env.DB_HOST as string,
-  port: Number(process.env.SERVER_PORT),
   database: process.env.DB_DATABASE as string,
   username: process.env.DB_USER as string,
   password: process.env.DB_PASSWORD as string,
