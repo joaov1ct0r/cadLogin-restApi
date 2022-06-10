3ea5309ac41755be28001aa69b94774e373c51cf
Joao Victor <jov9051@outlook.com>
2022-06-10 15:43:29 -0300

    feat: cors sendo utilizado pelo servidor

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import express from "express";
 
 import cookieParser from "cookie-parser";
 
+import cors from "cors";
+
 import userRouter from "./routes/userRoutes";
 
 import postRouter from "./routes/postRoutes";
@@ -11,6 +13,8 @@ import postRouter from "./routes/postRoutes";
 import adminRouter from "./routes/adminRoutes";
 
 const app: express.Express = express();
+
+app.use(cors());
 
 app.use(express.json());
 
