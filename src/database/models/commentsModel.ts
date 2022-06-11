901e8e5e7c450e8a8c16699b00adad268478e579
Joao Victor <jov9051@outlook.com>
2022-06-11 17:31:00 -0300

    feat: Comments exportado de commentsModel.ts

--- src/database/models/commentsModel.ts
+++ src/database/models/commentsModel.ts
@@ -5,3 +5,35 @@ import { ModelStatic, DataTypes } from "sequelize";
 import IComments from "../../types/commentsInterface";
 
 import Post from "./postModel";
+
+const Comments: ModelStatic<IComments> = DB.define(
+  "comments",
+  {
+    author: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+    },
+    comment: {
+      type: DataTypes.STRING(250),
+      allowNull: false,
+    },
+  },
+  {
+    freezeTableName: true,
+    tableName: "comments",
+    timestamps: false,
+  }
+);
+
+Comments.belongsTo(Post, {
+  constraints: true,
+  foreignKey: "postId",
+});
+
+Post.hasMany(Comments, {
+  foreignKey: "postId",
+});
+
+Comments.sync();
+
+export default Comments;
