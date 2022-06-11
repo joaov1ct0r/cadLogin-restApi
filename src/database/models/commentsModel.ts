cf6b5c1013d63c186e45aea28a65298c4ef3c967
Joao Victor <jov9051@outlook.com>
2022-06-11 17:27:26 -0300

    feat: IComments importado para commentsModel.ts

--- src/database/models/commentsModel.ts
+++ src/database/models/commentsModel.ts
@@ -2,4 +2,6 @@ import DB from "../config/database";
 
 import { ModelStatic, DataTypes } from "sequelize";
 
+import IComments from "../../types/commentsInterface";
+
 import Post from "./postModel";
