8cb238f6d27b88f00dd7557ab4eebc1896fc8ce2
Joao Victor <jov9051@outlook.com>
2022-07-26 16:18:47 -0300

    feat: App importado para index.ts e escutando reqs na porta SERVER_PORT

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,7 @@
 import "dotenv/config";
 
-import express from "express";
+import App from "./app";
 
-import cookieParser from "cookie-parser";
-
-import cors from "cors";
-
-import userRouter from "./routes/userRoutes";
-
-import postRouter from "./routes/postRoutes";
-
-import adminRouter from "./routes/adminRoutes";
-
-import swaggerUi from "swagger-ui-express";
-
-import swaggerDocs from "./swagger.json";
-
-const app: express.Express = express();
-
-app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
-app.use(cors());
-
-app.use(express.json());
-
-app.use(cookieParser());
-
-app.use(express.urlencoded({ extended: true }));
-
-app.use("/api/users", userRouter);
-
-app.use("/api/posts", postRouter);
-
-app.use("/api/admin", adminRouter);
-
-app.listen(process.env.SERVER_PORT, (): void => {
+new App().server.listen(process.env.SERVER_PORT, () => {
   console.log("Server running");
 });
