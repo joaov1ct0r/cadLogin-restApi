5e3cba693dcbc2d5c956204fdcaeeebb977bf151
Joao Victor <jov9051@outlook.com>
2022-07-28 21:17:24 -0300

    feat: createUserRequestInterface.ts criada

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 
 import CreateUserService from "../services/CreateUserService";
 
+import ICreateUserService from "../types/createUserServiceInterface";
+
 export default class CreateUserController {
   async execute(req: Request, res: Response): Promise<Response> {
     const email: string = req.body.email;
@@ -11,5 +13,9 @@ export default class CreateUserController {
     const name: string = req.body.name;
 
     const bornAt: string = req.body.bornAt;
+
+    const createUserService: ICreateUserService = new CreateUserController();
+
+    const user = await createUserService.execute(email, password, name, bornAt);
   }
 }
---
+++ src/types/createUserRequestInterface.ts
@@ -0,0 +1,6 @@
+export default interface ICreateUserRequest {
+  email: string;
+  password: string;
+  name: string;
+  bornAt: string;
+}
