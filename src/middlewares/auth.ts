e79cdedcadef2a9e0b8341031d858bfd161db9ff
Joao Victor <jov9051@outlook.com>
2022-07-30 18:08:07 -0300

    feat: auth.ts tipado

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -6,6 +6,10 @@ import IReq from "../interfaces/requestInterface";
 
 import { Response, NextFunction } from "express";
 
+import BadRequestError from "../errors/BadRequestError";
+
+import UnathorizedError from "../errors/UnathorizedError";
+
 export default function (
   req: IReq,
   res: Response,
@@ -13,25 +17,25 @@ export default function (
 ): Response | undefined {
   const token: string = req.cookies.authorization.split(" ")[1];
 
-  if (token.length === 0) {
-    return res.status(400).json({ error: "Token não encontrado!" });
-  }
-
   try {
+    if (token.length === 0) {
+      throw new BadRequestError("Token não encontrado!");
+    }
+
     const verifiedToken: IJwt = jwt.verify(
       token,
       process.env.JWT_TOKEN_SECRET as string
     ) as IJwt;
 
     if (!verifiedToken) {
-      return res.status(401).json({ error: "Falha na autenticação!" });
+      throw new UnathorizedError("Não autorizado!");
     }
     req.userId = verifiedToken.id;
 
     req.admin = verifiedToken.admin;
 
     next();
-  } catch (err: unknown) {
-    return res.status(500).json({ err });
+  } catch (err: any) {
+    return res.status(err.statusCode).json(err.message);
   }
 }
