cedefc226d4f62a84f4a73635060d60c6b753706
Joao Victor <jov9051@outlook.com>
2022-07-30 17:24:22 -0300

    refactor: app.ts tipado

--- src/app.ts
+++ src/app.ts
@@ -36,11 +36,11 @@ export default class App {
   }
 
   private userRoutes() {
-    this.server.use("/api/user", userRouter);
+    this.server.use("/api/users", userRouter);
   }
 
   private postRoutes() {
-    this.server.use("/api/post", postRouter);
+    this.server.use("/api/posts", postRouter);
   }
 
   private adminRoutes() {
