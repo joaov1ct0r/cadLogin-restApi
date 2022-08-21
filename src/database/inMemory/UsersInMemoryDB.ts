c13fe469eaf44d32e8d8158e46c55c02b29c8d5f
Joao Victor <jov9051@outlook.com>
2022-08-21 20:25:14 -0300

    feat: metodo exists criado em UsersInMemoryDB

--- src/database/inMemory/UsersInMemoryDB.ts
+++ src/database/inMemory/UsersInMemoryDB.ts
@@ -6,7 +6,12 @@ import IUser from "../../interfaces/IUser";
 
 export default class UsersInMemoryDB implements IUsersInMemoryDB {
   private users: IUser[] = [];
-  async exists(email: string): Promise<boolean> {}
+  async exists(email: string): Promise<boolean> {
+    const response = this.users.some((user) => user.email === email);
+
+    return response;
+  }
+
   async create(user: IUser): Promise<IUser> {
     Object.assign(user, {
       id: uuid(),
