b045659dbcc7936ab319cdedb1b74a3380e3ddfd
Joao Victor <jov9051@outlook.com>
2022-08-26 13:17:24 -0300

    test: CreateUserService.test.ts criado

--- __tests__/unit/CreateUserService.test.ts
+++ __tests__/unit/CreateUserService.test.ts
@@ -7,21 +7,26 @@ import ICreateUserService from "../../src/interfaces/ICreateUserService";
 describe("create user service", () => {
   describe("when execute is called", () => {
     it("should create a new user", async () => {
-      const userData = {
-        id: "1",
+      const userInputData = {
         email: "user@mail.com.br",
         password: "123123123",
         name: "user name",
         bornAt: "11/09/2001",
-      } as unknown as IUser;
+      };
+
+      const id = {
+        id: "1",
+      };
+
+      const returnedData = { ...userInputData, ...id };
 
       const mockRepository = mock<ICreateUserService>();
 
       mockRepository.execute.mockReturnValue(
-        userData as unknown as Promise<IUser>
+        returnedData as unknown as Promise<IUser>
       );
 
-      expect(mockRepository.execute(userData)).toHaveProperty("id");
+      expect(mockRepository.execute(userInputData)).toHaveProperty("id");
 
       expect(mockRepository.execute).toHaveBeenCalledTimes(1);
     });
