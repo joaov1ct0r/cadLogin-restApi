e8d021a5dd2c207c8bc281fd4cea9eb5390cf7a5
Joao Victor <jov9051@outlook.com>
2022-08-27 21:29:39 -0300

    test: CreateUserService.test.ts criado

--- __tests__/unit/CreateUserService.test.ts
+++ __tests__/unit/CreateUserService.test.ts
@@ -1,47 +1,49 @@
-import { jest } from "@jest/globals";
-
-import { CalledWithMock, mock } from "jest-mock-extended";
+import { mockDeep, mockClear } from "jest-mock-extended";
 
 import IUser from "../../src/interfaces/IUser";
 
+import User from "../../src/database/models/userModel";
+
+import CreateUserService from "../../src/services/CreateUserService";
+
 import ICreateUserService from "../../src/interfaces/ICreateUserService";
 
-import ICreateUserRequest from "../../src/interfaces/ICreateUserRequest";
+// import BadRequestError from "../../src/errors/BadRequestError";
 
-import BadRequestError from "../../src/errors/BadRequestError";
+import { ModelStatic } from "sequelize";
 
 describe("create user service", () => {
-  let mockRepository: {
-    execute: CalledWithMock<Promise<IUser>, [ICreateUserRequest]>;
-  } & ICreateUserService;
+  let mockRepository: ModelStatic<IUser>;
 
-  beforeEach(() => {
-    mockRepository = mock<ICreateUserService>();
-  });
-
-  afterEach(() => {
-    jest.clearAllMocks();
-  });
+  let sut: ICreateUserService;
 
   describe("when execute is called", () => {
-    it("should throw an error if user already exists", async () => {
-      const userInputData = {
-        email: "user@mail.com.br",
-        password: "123123123",
-        name: "user name",
-        bornAt: "11/09/2001",
-      };
+    beforeAll(() => {
+      mockRepository = mockDeep<typeof User>();
 
-      mockRepository.execute.mockRejectedValue(
-        new BadRequestError("Usuario já cadastrado!")
-      );
-
-      expect(async () => {
-        await mockRepository.execute(userInputData);
-      }).rejects.toThrow(new BadRequestError("Usuario já cadastrado!"));
-
-      expect(mockRepository.execute).toHaveBeenCalledTimes(1);
+      sut = new CreateUserService(mockRepository);
     });
+
+    beforeEach(() => {
+      mockClear(mockRepository);
+    });
+
+    // it("should throw an error if user already exists", async () => {
+    //   const userInputData = {
+    //     email: "user@email.com.br",
+    //     password: "123123123",
+    //     name: "user name",
+    //     bornAt: "11/09/2001",
+    //   };
+
+    //   // await sut.execute(userInputData);
+
+    //   expect(async () => {
+    //     await sut.execute(userInputData);
+    //   }).rejects.toThrow(new BadRequestError("Usuario já cadastrado!"));
+
+    //   expect(mockRepository.findOne).toHaveBeenCalledTimes(1);
+    // });
 
     it("should create a new user", async () => {
       const userInputData = {
@@ -51,19 +53,13 @@ describe("create user service", () => {
         bornAt: "11/09/2001",
       };
 
-      const id = {
-        id: "1",
-      };
+      const user = await sut.execute(userInputData);
 
-      const returnedData = { ...userInputData, ...id };
+      expect(user).toHaveProperty("id");
 
-      mockRepository.execute.mockReturnValue(
-        returnedData as unknown as Promise<IUser>
-      );
+      expect(mockRepository.findOne).toHaveBeenCalledTimes(1);
 
-      expect(mockRepository.execute(userInputData)).toHaveProperty("id");
-
-      expect(mockRepository.execute).toHaveBeenCalledTimes(1);
+      expect(mockRepository.create).toHaveBeenCalledTimes(1);
     });
   });
 });
