e80f370f209ad9648301d662092019d8d837c288
Joao Victor <jov9051@outlook.com>
2022-08-30 15:31:57 -0300

    test: teste para retorno de exception quando houver falha ao atualizar usuario, criado

--- __tests__/unit/services/EditUserService.test.ts
+++ __tests__/unit/services/EditUserService.test.ts
@@ -1,8 +1,39 @@
 import { mock } from "jest-mock-extended";
 
-import IUser from "../../../src/interfaces/IUser";
-
 import User from "../../../src/database/models/userModel";
 
 import EditUserService from "../../../src/services/EditUserService";
 
+import IEditUserService from "../../../src/interfaces/IEditUserService";
+
+import InternalError from "../../../src/errors/InternalError";
+
+const makeSut = () => {
+  const mockRepository = mock<typeof User>();
+
+  const sut: IEditUserService = new EditUserService(mockRepository);
+
+  return { mockRepository, sut };
+};
+
+describe("edit user service", () => {
+  describe("when execute is called", () => {
+    it("should throw exception if fail to edit user", async () => {
+      const { sut, mockRepository } = makeSut();
+
+      const userData = {
+        email: "user@mail.com.br",
+        password: "123123123",
+        name: "user name",
+        bornAt: "11/09/2001",
+        id: "1",
+      };
+
+      mockRepository.update.mockResolvedValueOnce([0]);
+
+      expect(async () => {
+        await sut.execute(userData);
+      }).rejects.toThrow(new InternalError("Falha ao atualizar usuario!"));
+    });
+  });
+});
