997c6eb2a78f2c144411181bf2cdf33ffa9f0db2
Joao Victor <jov9051@outlook.com>
2022-08-30 15:35:50 -0300

    test: teste para retorno de numero de linhas editadas ser maior que 0, criado

--- __tests__/unit/services/EditUserService.test.ts
+++ __tests__/unit/services/EditUserService.test.ts
@@ -35,5 +35,23 @@ describe("edit user service", () => {
         await sut.execute(userData);
       }).rejects.toThrow(new InternalError("Falha ao atualizar usuario!"));
     });
+
+    it("should return number of affected lines if succeed to edit user", async () => {
+      const { sut, mockRepository } = makeSut();
+
+      const userData = {
+        email: "user@mail.com.br",
+        password: "123123123",
+        name: "user name",
+        bornAt: "11/09/2001",
+        id: "1",
+      };
+
+      mockRepository.update.mockResolvedValueOnce([5]);
+
+      const editedLines = await sut.execute(userData);
+
+      expect(editedLines).toBeGreaterThan(0);
+    });
   });
 });
