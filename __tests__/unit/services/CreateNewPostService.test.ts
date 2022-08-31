97ca970c286c0963bcdfd3088d5eadb9308fd003
Joao Victor <jov9051@outlook.com>
2022-08-31 20:20:18 -0300

    test: teste para retorno de novo post criado

--- __tests__/unit/services/CreateNewPostService.test.ts
+++ __tests__/unit/services/CreateNewPostService.test.ts
@@ -36,5 +36,35 @@ describe("create new post service", () => {
         await sut.execute("1", "titulo para novo post");
       }).rejects.toThrow(new BadRequestError("Usuario não encontrado!"));
     });
+
+    it("should return a new post if succeed", async () => {
+      const { sut, mockRepository, mockUserRepository } = makeSut();
+
+      mockUserRepository.findOne.mockResolvedValueOnce({
+        id: "1",
+        email: "any@mail.com.br",
+        password: "123123123",
+        name: "user name",
+        bornAt: "11/09/2001",
+        admin: true,
+      } as IUser);
+
+      mockRepository.create.mockResolvedValueOnce({
+        id: "1",
+        author: "any@mail.com.br",
+        content: "novo titulo de post",
+        userId: "1",
+        likes: ["0"],
+        comments: ["0"],
+      } as IPost);
+
+      const post = await sut.execute("1", "novo titulo de post");
+
+      expect(post.author).toEqual("any@mail.com.br");
+
+      expect(post.content).toEqual("novo titulo de post");
+
+      expect(post).toHaveProperty("id");
+    });
   });
 });
