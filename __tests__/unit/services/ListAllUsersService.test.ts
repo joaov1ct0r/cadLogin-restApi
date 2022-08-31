95b254eb2fc2f00d5434925f0b0da3bea03e25a2
Joao Victor <jov9051@outlook.com>
2022-08-31 15:45:43 -0300

    test: teste para retorno de users criado

---
+++ __tests__/unit/services/ListAllUsersService.test.ts
@@ -0,0 +1,50 @@
+import { mock } from "jest-mock-extended";
+
+import { ModelStatic } from "sequelize";
+
+import IUser from "../../../src/interfaces/IUser";
+
+import IListAllUsersService from "../../../src/interfaces/IListAllUsersService";
+
+import ListAllUsersService from "../../../src/services/ListAllUsersService";
+
+const makeSut = () => {
+  const mockRepository = mock<ModelStatic<IUser>>();
+
+  const sut: IListAllUsersService = new ListAllUsersService(mockRepository);
+
+  return { mockRepository, sut };
+};
+
+describe("list all users service", () => {
+  describe("when execute is called", () => {
+    it("should return all users registered", async () => {
+      const { sut, mockRepository } = makeSut();
+
+      mockRepository.findAll.mockResolvedValueOnce([
+        {
+          id: "1",
+          email: "any@mail.com.br",
+          password: "123123123",
+          name: "user name",
+          bornAt: "11/09/2001",
+          admin: true,
+        } as IUser,
+        {
+          id: "1",
+          email: "any@mail.com.br",
+          password: "123123123",
+          name: "user name",
+          bornAt: "11/09/2001",
+          admin: true,
+        } as IUser,
+      ]);
+
+      const users = await sut.execute();
+
+      expect(users.length).toBe(2);
+
+      expect(users[0].email).toEqual("any@mail.com.br");
+    });
+  });
+});
