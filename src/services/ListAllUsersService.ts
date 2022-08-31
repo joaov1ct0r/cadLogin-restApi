58c422f8b31090be67d5cc08014ce87af0581180
Joao Victor <jov9051@outlook.com>
2022-08-31 15:39:19 -0300

    refactor: repository pattern adicionado para ListAllUsersService.ts

--- src/services/ListAllUsersService.ts
+++ src/services/ListAllUsersService.ts
@@ -1,14 +1,20 @@
-import User from "../database/models/userModel";
-
 import IUser from "../interfaces/IUser";
 
 import Post from "../database/models/postModel";
 
 import IListAllUsersService from "../interfaces/IListAllUsersService";
 
+import { ModelStatic } from "sequelize";
+
 export default class ListAllUsersService implements IListAllUsersService {
+  private readonly repository: ModelStatic<IUser>;
+
+  constructor(repository: ModelStatic<IUser>) {
+    this.repository = repository;
+  }
+
   public async execute(): Promise<IUser[]> {
-    const users: IUser[] = await User.findAll({
+    const users: IUser[] = await this.repository.findAll({
       include: Post,
     });
 
