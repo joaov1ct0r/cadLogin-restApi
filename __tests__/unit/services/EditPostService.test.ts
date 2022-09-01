ae01a48a07055d012e85b0b1c102f5e4de2536aa
Joao Victor <jov9051@outlook.com>
2022-08-31 21:59:38 -0300

    test: teste para retorno de exception quando houver falha ao editar post, criado

--- __tests__/unit/services/EditPostService.test.ts
+++ __tests__/unit/services/EditPostService.test.ts
@@ -7,7 +7,9 @@ import EditPostService from "../../../src/services/EditPostService";
 import IEditPostService from "../../../src/interfaces/IEditPostService";
 
 import { ModelStatic } from "sequelize";
+
 import BadRequestError from "../../../src/errors/BadRequestError";
+import InternalError from "../../../src/errors/InternalError";
 
 const makeSut = () => {
   const mockRepository = mock<ModelStatic<IPost>>();
@@ -27,6 +29,25 @@ describe("edit post service", () => {
       expect(async () => {
         await sut.execute("1", "1", "titulo editado para post");
       }).rejects.toThrow(new BadRequestError("Post não encontrado!"));
+    });
+
+    it("should return exception if failed to edit post", async () => {
+      const { sut, mockRepository } = makeSut();
+
+      mockRepository.findOne.mockResolvedValueOnce({
+        id: "1",
+        author: "any@mail.com.br",
+        content: "titulo de post",
+        userId: "1",
+        likes: ["0"],
+        comments: ["0"],
+      } as IPost);
+
+      mockRepository.update.mockResolvedValueOnce([0]);
+
+      expect(async () => {
+        await sut.execute("1", "1", "titulo editado de post");
+      }).rejects.toThrow(new InternalError("Falha ao atualizar Post!"));
     });
   });
 });
