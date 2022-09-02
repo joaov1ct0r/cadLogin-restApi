c315d22fb968bab5409ccad084bdacacf03a5554
Joao Victor <jov9051@outlook.com>
2022-09-02 17:04:20 -0300

    refactor: repository pattern adicionado a EditPostCommentService.ts

---
+++ __tests__/unit/services/EditPostCommentService.test.ts
@@ -0,0 +1,11 @@
+import { mock } from "jest-mock-extended";
+
+import IPost from "../../../src/interfaces/IPost";
+
+import IComments from "../../../src/interfaces/IComments";
+
+import IUser from "../../../src/interfaces/IUser";
+
+import IEditPostCommentService from "../../../src/interfaces/IEditPostCommentService";
+
+import EditPostCommentService from "../../../src/services/EditPostCommentService";
--- src/services/EditPostCommentService.ts
+++ src/services/EditPostCommentService.ts
@@ -1,16 +1,10 @@
-import Post from "../database/models/postModel";
-
 import IPost from "../interfaces/IPost";
 
 import BadRequestError from "../errors/BadRequestError";
 
-import Comments from "../database/models/commentsModel";
-
 import IComments from "../interfaces/IComments";
 
-import { Op } from "sequelize";
-
-import User from "../database/models/userModel";
+import { ModelStatic, Op } from "sequelize";
 
 import IUser from "../interfaces/IUser";
 
@@ -19,13 +13,31 @@ import InternalError from "../errors/InternalError";
 import IEditPostCommentService from "../interfaces/IEditPostCommentService";
 
 export default class EditPostCommentService implements IEditPostCommentService {
+  private readonly repository: ModelStatic<IPost>;
+
+  private readonly userRepository: ModelStatic<IUser>;
+
+  private readonly commentRepository: ModelStatic<IComments>;
+
+  constructor(
+    repository: ModelStatic<IPost>,
+    userRepository: ModelStatic<IUser>,
+    commentRepository: ModelStatic<IComments>
+  ) {
+    this.repository = repository;
+
+    this.userRepository = userRepository;
+
+    this.commentRepository = commentRepository;
+  }
+
   public async execute(
     userId: string | undefined,
     postId: string,
     commentId: string,
     comment: string
   ): Promise<number> {
-    const isPostRegistered: IPost | null = await Post.findOne({
+    const isPostRegistered: IPost | null = await this.repository.findOne({
       where: { id: postId },
     });
 
@@ -33,36 +45,8 @@ export default class EditPostCommentService implements IEditPostCommentService {
       throw new BadRequestError("Post não encontrado!");
     }
 
-    const isCommentRegistered: IComments | null = await Comments.findOne({
-      where: {
-        [Op.and]: [
-          {
-            postId,
-            userId,
-            id: commentId,
-          },
-        ],
-      },
-    });
-
-    if (isCommentRegistered === null) {
-      throw new BadRequestError("Comentario não encontrado!");
-    }
-
-    const user: IUser | null = await User.findOne({
-      where: {
-        id: userId,
-      },
-    });
-
-    const updatedComment: [affectedCount: number] = await Comments.update(
-      {
-        comment,
-        author: user!.email,
-        userId,
-        postId: isPostRegistered.id,
-      },
-      {
+    const isCommentRegistered: IComments | null =
+      await this.commentRepository.findOne({
         where: {
           [Op.and]: [
             {
@@ -72,8 +56,38 @@ export default class EditPostCommentService implements IEditPostCommentService {
             },
           ],
         },
-      }
-    );
+      });
+
+    if (isCommentRegistered === null) {
+      throw new BadRequestError("Comentario não encontrado!");
+    }
+
+    const user: IUser | null = await this.userRepository.findOne({
+      where: {
+        id: userId,
+      },
+    });
+
+    const updatedComment: [affectedCount: number] =
+      await this.commentRepository.update(
+        {
+          comment,
+          author: user!.email,
+          userId,
+          postId: isPostRegistered.id,
+        },
+        {
+          where: {
+            [Op.and]: [
+              {
+                postId,
+                userId,
+                id: commentId,
+              },
+            ],
+          },
+        }
+      );
 
     if (updatedComment[0] === 0) {
       throw new InternalError("Falha ao atualizar comentario!");
