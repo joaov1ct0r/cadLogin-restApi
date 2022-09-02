12e9b6db4f0ef175b21b77113c052815873e644d
Joao Victor <jov9051@outlook.com>
2022-09-02 19:46:55 -0300

    test: teste para retorno de exception quando post for null, criado

---
+++ __tests__/unit/services/DeletePostCommentService.test.ts
@@ -0,0 +1,42 @@
+import { mock } from "jest-mock-extended";
+
+import IPost from "../../../src/interfaces/IPost";
+
+import IComments from "../../../src/interfaces/IComments";
+
+import { ModelStatic } from "sequelize";
+
+import BadRequestError from "../../../src/errors/BadRequestError";
+
+import InternalError from "../../../src/errors/InternalError";
+
+import IDeletePostCommentService from "../../../src/interfaces/IDeletePostCommentService";
+
+import DeletePostCommentService from "../../../src/services/DeletePostCommentService";
+
+const makeSut = () => {
+  const mockRepository = mock<ModelStatic<IPost>>();
+
+  const mockCommentsRepository = mock<ModelStatic<IComments>>();
+
+  const sut: IDeletePostCommentService = new DeletePostCommentService(
+    mockRepository,
+    mockCommentsRepository
+  );
+
+  return { sut, mockRepository, mockCommentsRepository };
+};
+
+describe("delete post comment service", () => {
+  describe("when execute is called", () => {
+    it("should throw an exception if post is null", async () => {
+      const { sut, mockRepository } = makeSut();
+
+      mockRepository.findOne.mockResolvedValueOnce(null);
+
+      expect(async () => {
+        await sut.execute("1", "1", "1");
+      }).rejects.toThrow(new BadRequestError("Post não encontrado!"));
+    });
+  });
+});
