b829b16fc2591322279d28e1034eceeac2b2c79e
Joao Victor <jov9051@outlook.com>
2022-09-02 15:57:19 -0300

    test: teste para retorno de exception quando post for null, criado

--- __tests__/unit/services/DeletePostLikeService.test.ts
+++ __tests__/unit/services/DeletePostLikeService.test.ts
@@ -11,3 +11,32 @@ import BadRequestError from "../../../src/errors/BadRequestError";
 import InternalError from "../../../src/errors/InternalError";
 
 import IDeletePostLikeService from "../../../src/interfaces/IDeletePostLikeService";
+
+import DeletePostLikeService from "../../../src/services/DeletePostLikeService";
+
+const makeSut = () => {
+  const mockRepository = mock<ModelStatic<IPost>>();
+
+  const mockLikesRepository = mock<ModelStatic<ILikes>>();
+
+  const sut: IDeletePostLikeService = new DeletePostLikeService(
+    mockRepository,
+    mockLikesRepository
+  );
+
+  return { sut, mockLikesRepository, mockRepository };
+};
+
+describe("delete post like service", () => {
+  describe("when execute is called", () => {
+    it("should throw an exception if post is null", async () => {
+      const { sut, mockRepository } = makeSut();
+
+      mockRepository.findOne.mockResolvedValueOnce(null);
+
+      expect(async () => {
+        await sut.execute("1", "1");
+      }).rejects.toThrow(new BadRequestError("Post não encontrado!"));
+    });
+  });
+});
