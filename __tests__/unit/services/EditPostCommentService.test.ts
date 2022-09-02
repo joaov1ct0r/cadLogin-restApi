38b18f74282d29edcce61ba4b09c0ead635a657c
Joao Victor <jov9051@outlook.com>
2022-09-02 17:09:07 -0300

    test: teste para retorno de exception quando post for null, criado

--- __tests__/unit/services/EditPostCommentService.test.ts
+++ __tests__/unit/services/EditPostCommentService.test.ts
@@ -9,3 +9,37 @@ import IUser from "../../../src/interfaces/IUser";
 import IEditPostCommentService from "../../../src/interfaces/IEditPostCommentService";
 
 import EditPostCommentService from "../../../src/services/EditPostCommentService";
+
+import { ModelStatic } from "sequelize";
+
+import BadRequestError from "../../../src/errors/BadRequestError";
+
+const makeSut = () => {
+  const mockRepository = mock<ModelStatic<IPost>>();
+
+  const mockUserRepository = mock<ModelStatic<IUser>>();
+
+  const mockCommentsRepository = mock<ModelStatic<IComments>>();
+
+  const sut: IEditPostCommentService = new EditPostCommentService(
+    mockRepository,
+    mockUserRepository,
+    mockCommentsRepository
+  );
+
+  return { sut, mockRepository, mockCommentsRepository, mockUserRepository };
+};
+
+describe("edit post comment service", () => {
+  describe("when execute is called", () => {
+    it("should throw an exception if post is null", async () => {
+      const { sut, mockRepository } = makeSut();
+
+      mockRepository.findOne.mockResolvedValueOnce(null);
+
+      expect(async () => {
+        await sut.execute("1", "1", "1", "comment editado de post");
+      }).rejects.toThrow(new BadRequestError("Post não encontrado!"));
+    });
+  });
+});
