c49b8ef1f03467da8bccbed7369b974c0ec7f1f0
Joao Victor <jov9051@outlook.com>
2022-09-01 21:45:28 -0300

    test: teste para retorno de exception quando like ja estiver registrado, criado

--- __tests__/unit/services/AddPostLikeService.test.ts
+++ __tests__/unit/services/AddPostLikeService.test.ts
@@ -41,5 +41,39 @@ describe("add post like service", () => {
         await sut.execute("1", "1");
       }).rejects.toThrow(new BadRequestError("Post não encontrado!"));
     });
+
+    it("should throw an exception if like is already created", async () => {
+      const { sut, mockRepository, mockUserRepository, mockLikesRepository } =
+        makeSut();
+
+      mockRepository.findOne.mockResolvedValueOnce({
+        id: "1",
+        author: "any@mail.com.br",
+        content: "titulo de post",
+        userId: "1",
+        likes: ["0"],
+        comments: ["0"],
+      } as IPost);
+
+      mockUserRepository.findOne.mockResolvedValueOnce({
+        id: "1",
+        email: "any@mail.com.br",
+        password: "123123123",
+        name: "user name",
+        bornAt: "11/09/2001",
+        admin: true,
+      } as IUser);
+
+      mockLikesRepository.findOne.mockResolvedValueOnce({
+        postId: "1",
+        userId: "1",
+        id: "1",
+        likedBy: "any@mail.com.br",
+      } as ILikes);
+
+      expect(async () => {
+        await sut.execute("1", "1");
+      }).rejects.toThrow(new BadRequestError("Like já registrado!"));
+    });
   });
 });
