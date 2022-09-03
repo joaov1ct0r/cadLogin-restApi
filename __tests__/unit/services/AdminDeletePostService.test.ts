6b4e8e3ab512ef83053e5c74cab6c769229f2fcf
Joao Victor <jov9051@outlook.com>
2022-09-03 14:44:40 -0300

    test: teste para retorno de exception quando falhar para deletar post, criado

--- __tests__/unit/services/AdminDeletePostService.test.ts
+++ __tests__/unit/services/AdminDeletePostService.test.ts
@@ -10,6 +10,8 @@ import AdminDeletePostService from "../../../src/services/AdminDeletePostService
 
 import BadRequestError from "../../../src/errors/BadRequestError";
 
+import InternalError from "../../../src/errors/InternalError";
+
 const makeSut = () => {
   const mockRepository = mock<ModelStatic<IPost>>();
 
@@ -30,6 +32,25 @@ describe("admin delete post service", () => {
       expect(async () => {
         await sut.execute("1");
       }).rejects.toThrow(new BadRequestError("Post não encontrado!"));
+    });
+
+    it("should throw an exception if fails to delete post", async () => {
+      const { sut, mockRepository } = makeSut();
+
+      mockRepository.findOne.mockResolvedValueOnce({
+        id: "1",
+        author: "any@mail.com.br",
+        content: "titulo de post",
+        userId: "1",
+        likes: ["0"],
+        comments: ["0"],
+      } as IPost);
+
+      mockRepository.destroy.mockResolvedValueOnce(0);
+
+      expect(async () => {
+        await sut.execute("1");
+      }).rejects.toThrow(new InternalError("Falha ao deletar Post!"));
     });
   });
 });
