f4ff7528b846318536cdafb746f8a14fa354a8bd
Joao Victor <jov9051@outlook.com>
2022-09-05 19:40:03 -0300

    test: teste para retorno de exception quando data com parametros errados sao enviados

--- __tests__/integration/CreateUser.test.ts
+++ __tests__/integration/CreateUser.test.ts
@@ -9,6 +9,20 @@ describe("create user", () => {
     await truncate();
   });
 
+  it("should return an exception if wrong data is send", async () => {
+    const response = await request(new App().server)
+      .post("/api/users/register")
+      .set("Accept", "application/json")
+      .send({
+        email: "any@mail.com.br",
+        password: "123",
+        name: "user name",
+        bornAt: "01/09/2001",
+      });
+
+    expect(response.status).toEqual(400);
+  });
+
   it("should create a new user", async () => {
     const response = await request(new App().server)
       .post("/api/users/register")
