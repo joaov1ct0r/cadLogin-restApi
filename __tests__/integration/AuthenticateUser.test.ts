81949ca3d4692c0b50acab2addd1b2c4caf8d516
Joao Victor <jov9051@outlook.com>
2022-09-06 16:04:35 -0300

    test: teste para retorno de token ao autenticar, criado

--- __tests__/integration/AuthenticateUser.test.ts
+++ __tests__/integration/AuthenticateUser.test.ts
@@ -42,4 +42,30 @@ describe("authenticate user", () => {
 
     expect(response.status).toEqual(401);
   });
+
+  it("should return a token when authenticated", async () => {
+    await request(new App().server)
+      .post("/api/users/register")
+      .set("Accept", "application/json")
+      .send({
+        email: "usernewemail@mail.com.br",
+        password: "789789789",
+        name: "user name name",
+        bornAt: "01/09/2001",
+      });
+
+    const response = await request(new App().server)
+      .post("/api/users/login")
+      .set("Accept", "application/json")
+      .send({
+        email: "usernewemail@mail.com.br",
+        password: "789789789",
+      });
+
+    console.log(response.headers["set-cookie"][0]);
+
+    expect(response.headers["set-cookie"][0]).toBeDefined();
+
+    expect(response.status).toEqual(200);
+  });
 });
