f4bd1130e6a96b57bf554a572576329d908d59a5
Joao Victor <jov9051@outlook.com>
2022-09-06 15:29:03 -0300

    test: teste para retorno de exception quando passwords nao forem a mesma, criado

--- __tests__/integration/AuthenticateUser.test.ts
+++ __tests__/integration/AuthenticateUser.test.ts
@@ -20,4 +20,26 @@ describe("authenticate user", () => {
 
     expect(response.status).toEqual(400);
   });
+
+  it("should return an exception if passwords arent matching", async () => {
+    await request(new App().server)
+      .post("/api/users/register")
+      .set("Accept", "application/json")
+      .send({
+        email: "usernewemail@mail.com.br",
+        password: "789789789",
+        name: "user name name",
+        bornAt: "01/09/2001",
+      });
+
+    const response = await request(new App().server)
+      .post("/api/users/login")
+      .set("Accept", "application/json")
+      .send({
+        email: "usernewemail@mail.com.br",
+        password: "123123123",
+      });
+
+    expect(response.status).toEqual(401);
+  });
 });
