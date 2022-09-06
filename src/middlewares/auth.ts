4b2570763505cf82c7d1708d91ea63679070e708
Joao Victor <jov9051@outlook.com>
2022-09-06 19:09:53 -0300

    refactor: auth.ts refatorado

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -13,19 +13,12 @@ export default function (
 ): Response | undefined {
   const token: string = req.cookies.authorization.split(" ")[1];
 
-  if (token.length === 0) {
-    return res.status(400).json({ error: "Token não encontrado!" });
-  }
-
   try {
     const verifiedToken: IJwt = jwt.verify(
       token,
       process.env.JWT_TOKEN_SECRET as string
     ) as IJwt;
 
-    if (!verifiedToken) {
-      return res.status(401).json({ error: "Falha na autenticação!" });
-    }
     req.userId = verifiedToken.id;
 
     req.admin = verifiedToken.admin;
