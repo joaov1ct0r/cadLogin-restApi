368d36d0099b59666c75ae578f395321afbdf238
Joao Victor <jov9051@outlook.com>
2022-09-07 18:52:18 -0300

    test: teste para retorno de users, criado

---
+++ __tests__/integration/ListAllUsers.test.ts
@@ -0,0 +1,86 @@
+import { jest } from "@jest/globals";
+
+import truncate from "../utils/truncate";
+
+import App from "../../src/app";
+
+import request from "supertest";
+
+describe("list all users", () => {
+  beforeEach(async () => {
+    await truncate();
+
+    jest.setTimeout(30000);
+  });
+
+  it("should return an exception if not authenticated", async () => {
+    await request(new App().server)
+      .post("/api/users/register")
+      .set("Accept", "application/json")
+      .send({
+        email: "userlistingsall@mail.com.br",
+        password: "789789789",
+        name: "user name name",
+        bornAt: "01/09/2001",
+      });
+
+    const response = await request(new App().server)
+      .get("/api/users/users")
+      .send({
+        email: "userlistingsall@mail.com.br",
+        password: "123123123",
+        name: "user name name",
+        bornAt: "02/09/2001",
+      });
+
+    expect(response.status).toEqual(500);
+  });
+
+  it("should return all users registered", async () => {
+    await request(new App().server)
+      .post("/api/users/register")
+      .set("Accept", "application/json")
+      .send({
+        email: "userlisting12@mail.com.br",
+        password: "789789789",
+        name: "user name name",
+        bornAt: "01/09/2001",
+      });
+
+    await request(new App().server)
+      .post("/api/users/register")
+      .set("Accept", "application/json")
+      .send({
+        email: "userlisting1234@mail.com.br",
+        password: "789789789",
+        name: "user name name",
+        bornAt: "01/09/2001",
+      });
+
+    await request(new App().server)
+      .post("/api/users/register")
+      .set("Accept", "application/json")
+      .send({
+        email: "userlistedall@mail.com.br",
+        password: "789789789",
+        name: "user name name",
+        bornAt: "01/09/2001",
+      });
+
+    const loginRequest = await request(new App().server)
+      .post("/api/users/login")
+      .set("Accept", "application/json")
+      .send({
+        email: "userlistedall@mail.com.br",
+        password: "789789789",
+      });
+
+    const response = await request(new App().server)
+      .get("/api/users/users")
+      .set("Cookie", [loginRequest.headers["set-cookie"]]);
+
+    expect(response.status).toEqual(200);
+
+    expect(response.body.users).toBeDefined();
+  });
+});
