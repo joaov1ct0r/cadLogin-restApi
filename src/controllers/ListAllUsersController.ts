f471391400f406af38ac07a557ab731846e25d56
Joao Victor <jov9051@outlook.com>
2022-09-07 18:29:44 -0300

    refactor: repository pattern adicionado a ListAllUsersController.ts

--- src/controllers/ListAllUsersController.ts
+++ src/controllers/ListAllUsersController.ts
@@ -4,13 +4,17 @@ import ListAllUsersService from "../services/ListAllUsersService";
 
 import IListAllUsersService from "../interfaces/IListAllUsersService";
 
+import User from "../database/models/userModel";
+
 import IUser from "../interfaces/IUser";
 
 import IListAllUsersController from "../interfaces/IListAllUsersController";
 
 export default class ListAllUsersController implements IListAllUsersController {
   public async handle(req: Request, res: Response): Promise<Response> {
-    const listAllUsersService: IListAllUsersService = new ListAllUsersService();
+    const listAllUsersService: IListAllUsersService = new ListAllUsersService(
+      User
+    );
 
     try {
       const users: IUser[] = await listAllUsersService.execute();
