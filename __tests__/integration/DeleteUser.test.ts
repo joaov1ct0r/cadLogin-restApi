8a9aaeb025a36c68acd6ed61932f37dd686a7242
Joao Victor <jov9051@outlook.com>
2022-09-07 15:14:37 -0300

    test: teste para retorno de exception quando nao autenticado, criado

--- __tests__/integration/DeleteUser.test.ts
+++ __tests__/integration/DeleteUser.test.ts
@@ -12,4 +12,22 @@ describe("delete user", () => {
 
     jest.setTimeout(30000);
   });
+
+  it("should return an exception if not authenticated", async () => {
+    await request(new App().server)
+      .post("/api/users/register")
+      .set("Accept", "application/json")
+      .send({
+        email: "userdeleted@mail.com.br",
+        password: "789789789",
+        name: "user name name",
+        bornAt: "01/09/2001",
+      });
+
+    const response = await request(new App().server).delete(
+      "/api/users/delete"
+    );
+
+    expect(response.status).toEqual(500);
+  });
 });
