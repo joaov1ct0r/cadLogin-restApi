adcc1f9b4cad923ceeb5257ce25711a616d7298b
Joao Victor <jov9051@outlook.com>
2022-09-07 18:08:38 -0300

    test: teste para retorno de exception quando data com parametros errados sao enviados, criado

--- __tests__/integration/ListUser.test.ts
+++ __tests__/integration/ListUser.test.ts
@@ -35,4 +35,33 @@ describe("list user", () => {
 
     expect(response.status).toEqual(500);
   });
+
+  it("should return an exception if wrong data is send", async () => {
+    await request(new App().server)
+      .post("/api/users/register")
+      .set("Accept", "application/json")
+      .send({
+        email: "userlistingwrong@mail.com.br",
+        password: "789789789",
+        name: "user name name",
+        bornAt: "01/09/2001",
+      });
+
+    const login = await request(new App().server)
+      .post("/api/users/login")
+      .set("Accept", "application/json")
+      .send({
+        email: "userlistingwrong@mail.com.br",
+        password: "789789789",
+      });
+
+    const response = await request(new App().server)
+      .put("/api/users/edit")
+      .set("Cookie", [login.headers["set-cookie"]])
+      .send({
+        email: "a@mail.br",
+      });
+
+    expect(response.status).toEqual(400);
+  });
 });
