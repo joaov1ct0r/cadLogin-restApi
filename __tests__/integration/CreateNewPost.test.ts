b4cb937bf68129e6d5cb9c3a8cbe92f511191c56
Joao Victor <jov9051@outlook.com>
2022-09-08 18:25:33 -0300

    test: teste para criacao de post, criado

--- __tests__/integration/CreateNewPost.test.ts
+++ __tests__/integration/CreateNewPost.test.ts
@@ -54,12 +54,43 @@ describe("create new post", () => {
       });
 
     const response = await request(new App().server)
-      .get("/api/users/user")
+      .post("/api/posts/register")
       .set("Cookie", [login.headers["set-cookie"]])
       .send({
         content: "",
       });
 
     expect(response.status).toEqual(400);
+  });
+
+  it("should create a new post", async () => {
+    await request(new App().server)
+      .post("/api/users/register")
+      .set("Accept", "application/json")
+      .send({
+        email: "usercreatingpost@mail.com.br",
+        password: "789789789",
+        name: "user name name",
+        bornAt: "01/09/2001",
+      });
+
+    const login = await request(new App().server)
+      .post("/api/users/login")
+      .set("Accept", "application/json")
+      .send({
+        email: "usercreatingpost@mail.com.br",
+        password: "789789789",
+      });
+
+    const response = await request(new App().server)
+      .post("/api/posts/register")
+      .set("Cookie", [login.headers["set-cookie"]])
+      .send({
+        content: "novo post",
+      });
+
+    expect(response.status).toEqual(201);
+
+    expect(response.body.post).toHaveProperty("id");
   });
 });
