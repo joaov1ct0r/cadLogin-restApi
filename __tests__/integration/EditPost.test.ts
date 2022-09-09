17be2ac979b611198c4270947fbdc3d906a7fdeb
Joao Victor <jov9051@outlook.com>
2022-09-08 21:21:45 -0300

    test: teste para retorno de exception quando data com parametros errados sao enviados, criado

--- __tests__/integration/EditPost.test.ts
+++ __tests__/integration/EditPost.test.ts
@@ -63,4 +63,34 @@ describe("edit post", () => {
 
     expect(response.status).toEqual(400);
   });
+
+  it("should return an exception if post is not registered", async () => {
+    await request(new App().server)
+      .post("/api/users/register")
+      .set("Accept", "application/json")
+      .send({
+        email: "userwithoutregpost@mail.com",
+        password: "789789789",
+        name: "user name name",
+        bornAt: "01/09/2001",
+      });
+
+    const login = await request(new App().server)
+      .post("/api/users/login")
+      .set("Accept", "application/json")
+      .send({
+        email: "userwithoutregpost@mail.com",
+        password: "789789789",
+      });
+
+    const response = await request(new App().server)
+      .put("/api/posts/edit")
+      .set("Cookie", [login.headers["set-cookie"]])
+      .send({
+        content: "titulo de post editado",
+        postId: "184",
+      });
+
+    expect(response.status).toEqual(400);
+  });
 });
