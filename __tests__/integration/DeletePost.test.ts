62869cb7812af2f668f83e082422c0a7f5087886
Joao Victor <jov9051@outlook.com>
2022-09-10 12:54:04 -0300

    test: teste para retorno de exception quando data com parametros errados sao enviados, criado

--- __tests__/integration/DeletePost.test.ts
+++ __tests__/integration/DeletePost.test.ts
@@ -30,4 +30,40 @@ describe("delete post", () => {
 
     expect(response.status).toEqual(500);
   });
+
+  it("should return an exception if wrong data is send", async () => {
+    await request(new App().server)
+      .post("/api/users/register")
+      .set("Accept", "application/json")
+      .send({
+        email: "userdel345@mail.com.br",
+        password: "789789789",
+        name: "user name name",
+        bornAt: "01/09/2001",
+      });
+
+    const login = await request(new App().server)
+      .post("/api/users/login")
+      .set("Accept", "application/json")
+      .send({
+        email: "userdel345@mail.com.br",
+        password: "789789789",
+      });
+
+    await request(new App().server)
+      .post("/api/posts/register")
+      .set("Cookie", [login.headers["set-cookie"]])
+      .send({
+        content: "novo post",
+      });
+
+    const response = await request(new App().server)
+      .delete("/api/posts/delete")
+      .set("Cookie", [login.headers["set-cookie"]])
+      .send({
+        postId: "",
+      });
+
+    expect(response.status).toEqual(400);
+  });
 });
