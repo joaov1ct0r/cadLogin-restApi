1ff4d29536e7b389545e8f86a49009362eb2e44d
Joao Victor <jov9051@outlook.com>
2022-09-10 15:51:16 -0300

    test: teste para retorno de exception quando post for null, criado

--- __tests__/integration/AddPostLike.test.ts
+++ __tests__/integration/AddPostLike.test.ts
@@ -69,4 +69,33 @@ describe("add post like", () => {
 
     expect(response.status).toEqual(400);
   });
+
+  it("should return an exception if post is not registered", async () => {
+    await request(new App().server)
+      .post("/api/users/register")
+      .set("Accept", "application/json")
+      .send({
+        email: "tyuserptlk56@mail.com.br",
+        password: "789789789",
+        name: "user name name",
+        bornAt: "01/09/2001",
+      });
+
+    const login = await request(new App().server)
+      .post("/api/users/login")
+      .set("Accept", "application/json")
+      .send({
+        email: "tyuserptlk56@mail.com.br",
+        password: "789789789",
+      });
+
+    const response = await request(new App().server)
+      .post("/api/posts/like")
+      .set("Cookie", [login.headers["set-cookie"]])
+      .send({
+        postId: "204",
+      });
+
+    expect(response.status).toEqual(400);
+  });
 });
