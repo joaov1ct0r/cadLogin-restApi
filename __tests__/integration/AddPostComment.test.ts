238a318e8b8eb727de60b3718a89ac29def3c714
Joao Victor <jov9051@outlook.com>
2022-09-10 21:47:40 -0300

    test: teste para criacao de comment em post, criado

--- __tests__/integration/AddPostComment.test.ts
+++ __tests__/integration/AddPostComment.test.ts
@@ -98,4 +98,41 @@ describe("add post comment", () => {
 
     expect(response.status).toEqual(400);
   });
+
+  it("should create a comment in post", async () => {
+    await request(new App().server)
+      .post("/api/users/register")
+      .set("Accept", "application/json")
+      .send({
+        email: "user4238029843@mail.com.br",
+        password: "789789789",
+        name: "user name name",
+        bornAt: "01/09/2001",
+      });
+
+    const login = await request(new App().server)
+      .post("/api/users/login")
+      .set("Accept", "application/json")
+      .send({
+        email: "user4238029843@mail.com.br",
+        password: "789789789",
+      });
+
+    const postCreated = await request(new App().server)
+      .post("/api/posts/register")
+      .set("Cookie", [login.headers["set-cookie"]])
+      .send({
+        content: "novo post",
+      });
+
+    const response = await request(new App().server)
+      .post("/api/posts/comment")
+      .set("Cookie", [login.headers["set-cookie"]])
+      .send({
+        postId: String(postCreated.body.post.id),
+        comment: "novo comment",
+      });
+
+    expect(response.status).toEqual(201);
+  });
 });
