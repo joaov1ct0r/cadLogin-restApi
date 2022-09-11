2bbeec03081631b0249afab6985e655b7d247ac2
Joao Victor <jov9051@outlook.com>
2022-09-11 19:52:27 -0300

    test: teste para retorno de post, criado

--- __tests__/integration/ListPost.test.ts
+++ __tests__/integration/ListPost.test.ts
@@ -95,4 +95,40 @@ describe("list post", () => {
 
     expect(response.status).toEqual(400);
   });
+
+  it("should return an post", async () => {
+    await request(new App().server)
+      .post("/api/users/register")
+      .set("Accept", "application/json")
+      .send({
+        email: "user8420384208432@mail.com.br",
+        password: "789789789",
+        name: "user name name",
+        bornAt: "01/09/2001",
+      });
+
+    const login = await request(new App().server)
+      .post("/api/users/login")
+      .set("Accept", "application/json")
+      .send({
+        email: "user8420384208432@mail.com.br",
+        password: "789789789",
+      });
+
+    const postCreated = await request(new App().server)
+      .post("/api/posts/register")
+      .set("Cookie", [login.headers["set-cookie"]])
+      .send({
+        content: "novo post",
+      });
+
+    const response = await request(new App().server)
+      .get("/api/posts/post")
+      .set("Cookie", [login.headers["set-cookie"]])
+      .send({
+        postId: String(postCreated.body.post.id),
+      });
+
+    expect(response.status).toEqual(200);
+  });
 });
