be7f1587604c22b5fb323b6419fdac3dc80d119f
Joao Victor <jov9051@outlook.com>
2022-09-11 19:45:49 -0300

    test: teste para retorno de exception quando data com parametros errados sao enviados, criado

--- __tests__/integration/ListPost.test.ts
+++ __tests__/integration/ListPost.test.ts
@@ -28,9 +28,42 @@ describe("list post", () => {
       .get("/api/posts/post")
       .set("Accept", "application/json")
       .send({
-        postId: "",
+        postId: "290",
       });
 
     expect(response.status).toEqual(500);
+  });
+
+  it("should return an exception if wrong data is send", async () => {
+    await request(new App().server)
+      .post("/api/users/register")
+      .set("Accept", "application/json")
+      .send({
+        email: "userjljsfljsl5403@mail.com.br",
+        password: "789789789",
+        name: "user name name",
+        bornAt: "01/09/2001",
+      });
+
+    const login = await request(new App().server)
+      .post("/api/users/login")
+      .set("Accept", "application/json")
+      .send({
+        email: "userjljsfljsl5403@mail.com.br",
+        password: "789789789",
+      });
+
+    await request(new App().server)
+      .post("/api/posts/register")
+      .set("Cookie", [login.headers["set-cookie"]])
+      .send({
+        content: "novo post",
+      });
+
+    const response = await request(new App().server)
+      .get("/api/posts/post")
+      .set("Cookie", [login.headers["set-cookie"]]);
+
+    expect(response.status).toEqual(400);
   });
 });
