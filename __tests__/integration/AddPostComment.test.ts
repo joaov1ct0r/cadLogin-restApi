04e24025f65c1bd90ed06f253d42d171abcba85a
Joao Victor <jov9051@outlook.com>
2022-09-10 21:43:32 -0300

    test: teste para retorno de exception quando post for null, criado

--- __tests__/integration/AddPostComment.test.ts
+++ __tests__/integration/AddPostComment.test.ts
@@ -69,4 +69,33 @@ describe("add post comment", () => {
 
     expect(response.status).toEqual(400);
   });
+
+  it("should return an exception if post isnt null", async () => {
+    await request(new App().server)
+      .post("/api/users/register")
+      .set("Accept", "application/json")
+      .send({
+        email: "user92483279847239@mail.com.br",
+        password: "789789789",
+        name: "user name name",
+        bornAt: "01/09/2001",
+      });
+
+    const login = await request(new App().server)
+      .post("/api/users/login")
+      .set("Accept", "application/json")
+      .send({
+        email: "user92483279847239@mail.com.br",
+        password: "789789789",
+      });
+
+    const response = await request(new App().server)
+      .post("/api/posts/comment")
+      .set("Cookie", [login.headers["set-cookie"]])
+      .send({
+        postId: "290",
+      });
+
+    expect(response.status).toEqual(400);
+  });
 });
