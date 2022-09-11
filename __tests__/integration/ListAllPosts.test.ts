867695e5cd973cdb7c891772dc991d6c0c45b27c
Joao Victor <jov9051@outlook.com>
2022-09-11 20:10:07 -0300

    test: teste para retorno de posts, criado

--- __tests__/integration/ListAllPosts.test.ts
+++ __tests__/integration/ListAllPosts.test.ts
@@ -33,4 +33,30 @@ describe("list all posts", () => {
 
     expect(response.status).toEqual(500);
   });
+
+  it("should return an post", async () => {
+    await request(new App().server)
+      .post("/api/users/register")
+      .set("Accept", "application/json")
+      .send({
+        email: "userjsflsjl234235@mail.com.br",
+        password: "789789789",
+        name: "user name name",
+        bornAt: "01/09/2001",
+      });
+
+    const login = await request(new App().server)
+      .post("/api/users/login")
+      .set("Accept", "application/json")
+      .send({
+        email: "userjsflsjl234235@mail.com.br",
+        password: "789789789",
+      });
+
+    const response = await request(new App().server)
+      .get("/api/posts/posts")
+      .set("Cookie", [login.headers["set-cookie"]]);
+
+    expect(response.status).toEqual(200);
+  });
 });
