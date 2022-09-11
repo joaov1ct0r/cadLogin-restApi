944e25001edf76d14dfd6cfa6987b282e5d97a9e
Joao Victor <jov9051@outlook.com>
2022-09-11 19:28:36 -0300

    test: teste para retorno de exception quando post for null, criado

--- __tests__/integration/DeletePostComment.test.ts
+++ __tests__/integration/DeletePostComment.test.ts
@@ -74,4 +74,34 @@ describe("delete post comment", () => {
 
     expect(response.status).toEqual(400);
   });
+
+  it("should return an exception if post is null", async () => {
+    await request(new App().server)
+      .post("/api/users/register")
+      .set("Accept", "application/json")
+      .send({
+        email: "user48203482lkasf@mail.com.br",
+        password: "789789789",
+        name: "user name name",
+        bornAt: "01/09/2001",
+      });
+
+    const login = await request(new App().server)
+      .post("/api/users/login")
+      .set("Accept", "application/json")
+      .send({
+        email: "user48203482lkasf@mail.com.br",
+        password: "789789789",
+      });
+
+    const response = await request(new App().server)
+      .delete("/api/posts/comment/delete")
+      .set("Cookie", [login.headers["set-cookie"]])
+      .send({
+        commentId: "290",
+        postId: "290",
+      });
+
+    expect(response.status).toEqual(400);
+  });
 });
