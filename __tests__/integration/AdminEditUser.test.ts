5c9c3a2d5775ccc63ffca9d8eabd0547ae3c1128
Joao Victor <jov9051@outlook.com>
2022-09-12 18:36:02 -0300

    test: teste para retorno de exception quando nao admin, criado

--- __tests__/integration/AdminEditUser.test.ts
+++ __tests__/integration/AdminEditUser.test.ts
@@ -33,4 +33,37 @@ describe("admin edit user", () => {
 
     expect(response.status).toEqual(500);
   });
+
+  it("should return an exception if not admin", async () => {
+    await request(new App().server)
+      .post("/api/users/register")
+      .set("Accept", "application/json")
+      .send({
+        email: "userlkjsvxljvlkxdr4235@mail.com.br",
+        password: "789789789",
+        name: "user name name",
+        bornAt: "01/09/2001",
+      });
+
+    const login = await request(new App().server)
+      .post("/api/users/login")
+      .set("Accept", "application/json")
+      .send({
+        email: "userlkjsvxljvlkxdr4235@mail.com.br",
+        password: "789789789",
+      });
+
+    const response = await request(new App().server)
+      .put("/api/admin/user/edit")
+      .set("Cookie", [login.headers["set-cookie"]])
+      .send({
+        userEmail: "user58304vlxjl@mail.com.br",
+        userNewEmail: "use94234758937394@mail.com.br",
+        userNewPassword: "123123123",
+        userNewName: "name name user",
+        userNewBornAt: "05/09/2001",
+      });
+
+    expect(response.status).toEqual(401);
+  });
 });
