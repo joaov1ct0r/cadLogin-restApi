1e34698e7e8d7a446ae2ebd9f5d44a5484d0d78a
Joao Victor <jov9051@outlook.com>
2022-09-12 18:29:20 -0300

    test: teste para retorno de exception quando nao autenticado, criado

---
+++ __tests__/integration/AdminEditUser.test.ts
@@ -0,0 +1,36 @@
+import { jest } from "@jest/globals";
+
+import truncate from "../utils/truncate";
+
+import App from "../../src/app";
+
+import request from "supertest";
+
+describe("admin edit user", () => {
+  beforeEach(async () => {
+    await truncate();
+
+    jest.setTimeout(70000);
+  });
+
+  it("should return an exception if not authenticated", async () => {
+    await request(new App().server)
+      .post("/api/users/register")
+      .set("Accept", "application/json")
+      .send({
+        email: "user432840375lksjdlf@mail.com.br",
+        password: "789789789",
+        name: "user name name",
+        bornAt: "01/09/2001",
+      });
+
+    const response = await request(new App().server)
+      .put("/api/admin/user/edit")
+      .set("Accept", "application/json")
+      .send({
+        userEmail: "user432840375lksjdlf@mail.com.br",
+      });
+
+    expect(response.status).toEqual(500);
+  });
+});
