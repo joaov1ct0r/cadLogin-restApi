f2cbadf25151051b1d3aa37492eaa717cbad66c6
Joao Victor <jov9051@outlook.com>
2022-09-12 19:02:23 -0300

    test: teste para retorno de exception quando nao admin, criado

---
+++ __tests__/integration/AdminDeletePost.test.ts
@@ -0,0 +1,65 @@
+import { jest } from "@jest/globals";
+
+import truncate from "../utils/truncate";
+
+import App from "../../src/app";
+
+import request from "supertest";
+
+describe("admin edit user", () => {
+  beforeEach(async () => {
+    await truncate();
+
+    jest.setTimeout(70000);
+  });
+
+  it("should return an exception if not authenticated", async () => {
+    await request(new App().server)
+      .post("/api/users/register")
+      .set("Accept", "application/json")
+      .send({
+        email: "admin4723892793@mail.com.br",
+        password: "789789789",
+        name: "user name name",
+        bornAt: "01/09/2001",
+      });
+
+    const response = await request(new App().server)
+      .delete("/api/admin/user/delete")
+      .set("Accept", "application/json")
+      .send({
+        userEmail: "user432840375lksjdlf@mail.com.br",
+      });
+
+    expect(response.status).toEqual(500);
+  });
+
+  it("should return an exception if not admin", async () => {
+    await request(new App().server)
+      .post("/api/users/register")
+      .set("Accept", "application/json")
+      .send({
+        email: "user482034982042@mail.com.br",
+        password: "789789789",
+        name: "user name name",
+        bornAt: "01/09/2001",
+      });
+
+    const login = await request(new App().server)
+      .post("/api/users/login")
+      .set("Accept", "application/json")
+      .send({
+        email: "user482034982042@mail.com.br",
+        password: "789789789",
+      });
+
+    const response = await request(new App().server)
+      .delete("/api/admin/post/delete")
+      .set("Cookie", [login.headers["set-cookie"]])
+      .send({
+        postId: "290",
+      });
+
+    expect(response.status).toEqual(401);
+  });
+});
