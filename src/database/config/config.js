9d4e0b609d3d2e27286522014db3bbfd17a53304
Joao Victor <jov9051@outlook.com>
2022-09-22 21:59:36 -0300

    chore: config.js

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -5,7 +5,7 @@ export default {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT || "postgres",
     logging: true,
