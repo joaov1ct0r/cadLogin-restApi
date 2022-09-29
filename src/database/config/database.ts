d324ea818aa8ddee027c18de0ef30a44307369a7
Joao Victor <jov9051@outlook.com>
2022-09-29 17:32:26 -0300

    feat: authenticate adicionado a index.ts

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -1,18 +1,19 @@
 import sequelize, { Dialect } from "sequelize";
 
-const DB: sequelize.Sequelize = new sequelize.Sequelize({
-  dialect: "postgres" as Dialect,
-  host:
-    process.env.NODE_ENV === "production"
-      ? (process.env.DB_HOST as string)
-      : (process.env.DB_HOST_DEV as string),
-  database:
-    process.env.NODE_ENV === "test"
-      ? (process.env.DB_DATABASE_TEST as string)
-      : (process.env.DB_DATABASE as string),
-  username: process.env.DB_USER as string,
-  password: process.env.DB_PASSWORD as string,
-  logging: true,
-});
+const DB: sequelize.Sequelize = new sequelize.Sequelize(
+  process.env.NODE_ENV! === "test"
+    ? process.env.DB_DATABASE_TEST!
+    : process.env.DB_DATABASE!,
+  process.env.DB_USER!,
+  process.env.DB_PASSWORD!,
+  {
+    host:
+      process.env.NODE_ENV! === "production"
+        ? process.env.DB_HOST!
+        : process.env.DB_HOST_DEV!,
+    dialect: "postgres" as Dialect,
+    logging: true,
+  }
+);
 
 export default DB;
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,16 @@ import "dotenv/config";
 
 import App from "./app";
 
-new App().server.listen(process.env.SERVER_PORT, () => {
+import DB from "./database/config/database";
+
+new App().server.listen(process.env.SERVER_PORT, async () => {
   console.log("Server running");
+
+  try {
+    await DB.authenticate();
+
+    console.log("DB Connected!!");
+  } catch (error: any) {
+    throw new Error(error);
+  }
 });
