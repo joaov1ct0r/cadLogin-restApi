8632ee5e62a69ce1b67a178e2c229b8e7c61ef3b
Joao Victor <jov9051@outlook.com>
2022-11-15 12:09:01 -0300

    refactor: ListUserService.test.ts refatorado

--- __tests__/unit/services/ListUserService.test.ts
+++ __tests__/unit/services/ListUserService.test.ts
@@ -1,45 +1,39 @@
-import { mock } from "jest-mock-extended";
-
-import { ModelStatic } from "sequelize";
-
-import IUser from "../../../src/interfaces/IUser";
-
-import IListUserService from "../../../src/interfaces/IListUserService";
-
+import { mockDeep } from "jest-mock-extended";
 import ListUserService from "../../../src/services/ListUserService";
-
 import BadRequestError from "../../../src/errors/BadRequestError";
+import { PrismaClient } from "@prisma/client";
 
 const makeSut = () => {
-  const mockRepository = mock<ModelStatic<IUser>>();
+  const prismaSpyRepository = mockDeep<PrismaClient>();
 
-  const sut: IListUserService = new ListUserService(mockRepository);
+  const sut: ListUserService = new ListUserService(prismaSpyRepository);
 
-  return { mockRepository, sut };
+  return { prismaSpyRepository, sut };
 };
 
 describe("list user service", () => {
   describe("when execute is called", () => {
     it("should return an exception if user is null", async () => {
-      const { sut, mockRepository } = makeSut();
+      const { sut, prismaSpyRepository } = makeSut();
 
-      mockRepository.findOne.mockResolvedValueOnce(null);
+      prismaSpyRepository.user.findUnique.mockResolvedValueOnce(null);
 
       expect(async () => {
         await sut.execute("any@mail.com.br");
       }).rejects.toThrow(new BadRequestError("Usuario não encontrado!"));
     });
 
-    it("should return a user is execute is succeed", async () => {
-      const { sut, mockRepository } = makeSut();
+    it("should return a user if execute is succeed", async () => {
+      const { sut, prismaSpyRepository } = makeSut();
 
-      mockRepository.findOne.mockResolvedValueOnce({
-        id: "1",
+      prismaSpyRepository.user.findUnique.mockResolvedValueOnce({
+        id: 1,
         email: "any@mail.com.br",
+        name: "user name",
         password: "123123123",
         bornAt: "01/09/2001",
-        admin: true,
-      } as IUser);
+        admin: false,
+      });
 
       const user = await sut.execute("any@mail.com.br");
 
