66adae007fedd77c25c04408d32785f14fd61a0a
Joao Victor <jov9051@outlook.com>
2022-11-14 21:57:46 -0300

    refactor: CreateNewPostService.test.ts refatorado

--- __tests__/unit/services/CreateNewPostService.test.ts
+++ __tests__/unit/services/CreateNewPostService.test.ts
@@ -1,64 +1,50 @@
-import { mock } from "jest-mock-extended";
-
-import IPost from "../../../src/interfaces/IPost";
-
-import IUser from "../../../src/interfaces/IUser";
-
-import { ModelStatic } from "sequelize";
-
-import ICreateNewPostService from "../../../src/interfaces/ICreateNewPostService";
-
+import { mockDeep } from "jest-mock-extended";
 import CreateNewPostService from "../../../src/services/CreateNewPostService";
-
 import BadRequestError from "../../../src/errors/BadRequestError";
+import { PrismaClient } from "@prisma/client";
 
 const makeSut = () => {
-  const mockRepository = mock<ModelStatic<IPost>>();
+  const prismaSpyRepository = mockDeep<PrismaClient>();
 
-  const mockUserRepository = mock<ModelStatic<IUser>>();
-
-  const sut: ICreateNewPostService = new CreateNewPostService(
-    mockRepository,
-    mockUserRepository
+  const sut: CreateNewPostService = new CreateNewPostService(
+    prismaSpyRepository
   );
 
-  return { sut, mockRepository, mockUserRepository };
+  return { sut, prismaSpyRepository };
 };
 
 describe("create new post service", () => {
   describe("when execute is called", () => {
     it("should throw an exception if user is null", async () => {
-      const { sut, mockUserRepository } = makeSut();
+      const { sut, prismaSpyRepository } = makeSut();
 
-      mockUserRepository.findOne.mockResolvedValueOnce(null);
+      prismaSpyRepository.user.findUnique.mockResolvedValueOnce(null);
 
       expect(async () => {
-        await sut.execute("1", "titulo para novo post");
+        await sut.execute(1, "titulo para novo post");
       }).rejects.toThrow(new BadRequestError("Usuario não encontrado!"));
     });
 
     it("should return a new post if succeed", async () => {
-      const { sut, mockRepository, mockUserRepository } = makeSut();
+      const { sut, prismaSpyRepository } = makeSut();
 
-      mockUserRepository.findOne.mockResolvedValueOnce({
-        id: "1",
+      prismaSpyRepository.user.findUnique.mockResolvedValueOnce({
+        id: 1,
         email: "any@mail.com.br",
         password: "123123123",
         name: "user name",
         bornAt: "01/09/2001",
         admin: true,
-      } as IUser);
+      });
 
-      mockRepository.create.mockResolvedValueOnce({
-        id: "1",
+      prismaSpyRepository.post.create.mockResolvedValueOnce({
+        id: 1,
         author: "any@mail.com.br",
         content: "novo titulo de post",
-        userId: "1",
-        likes: ["0"],
-        comments: ["0"],
-      } as IPost);
+        userId: 1,
+      });
 
-      const post = await sut.execute("1", "novo titulo de post");
+      const post = await sut.execute(1, "novo titulo de post");
 
       expect(post.author).toEqual("any@mail.com.br");
 
