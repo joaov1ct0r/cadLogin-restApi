ab99ee957c207b452f9fd127ae49dea1564e5980
Joao Victor <jov9051@outlook.com>
2023-01-12 21:33:17 -0300

    feat: class ValidatePost criado

--- src/validations/validatePostData.ts
+++ src/validations/validatePostData.ts
@@ -1,92 +1,80 @@
 import Joi from "@hapi/joi";
 
-const validateHandleNewPost = (data: Object): Joi.ValidationResult => {
-  const schema: Joi.ObjectSchema<Object> = Joi.object({
-    content: Joi.string().required().min(1).max(250),
-  });
+export default class ValidatePost {
+  validateHandleNewPost(data: object): Joi.ValidationResult {
+    const schema: Joi.ObjectSchema<Object> = Joi.object({
+      content: Joi.string().required().min(1).max(250),
+    });
 
-  return schema.validate(data);
-};
+    return schema.validate(data);
+  }
 
-const validateHandleEditPost = (data: Object): Joi.ValidationResult => {
-  const schema: Joi.ObjectSchema<Object> = Joi.object({
-    postId: Joi.string().required().min(1),
-    content: Joi.string().required().min(1).max(250),
-  });
+  validateHandleEditPost(data: object): Joi.ValidationResult {
+    const schema: Joi.ObjectSchema<Object> = Joi.object({
+      postId: Joi.string().required().min(1),
+      content: Joi.string().required().min(1).max(250),
+    });
 
-  return schema.validate(data);
-};
+    return schema.validate(data);
+  }
 
-const validateHandleDeletePost = (data: Object): Joi.ValidationResult => {
-  const schema: Joi.ObjectSchema<Object> = Joi.object({
-    postId: Joi.string().required().min(1),
-  });
+  validateHandleDeletePost(data: object): Joi.ValidationResult {
+    const schema: Joi.ObjectSchema<Object> = Joi.object({
+      postId: Joi.string().required().min(1),
+    });
 
-  return schema.validate(data);
-};
+    return schema.validate(data);
+  }
 
-const validateHandleOnePost = (data: Object): Joi.ValidationResult => {
-  const schema: Joi.ObjectSchema<Object> = Joi.object({
-    postId: Joi.string().required().min(1),
-  });
+  validateHandleOnePost(data: object): Joi.ValidationResult {
+    const schema: Joi.ObjectSchema<Object> = Joi.object({
+      postId: Joi.string().required().min(1),
+    });
 
-  return schema.validate(data);
-};
+    return schema.validate(data);
+  }
 
-const validateHandleAddPostLike = (data: Object): Joi.ValidationResult => {
-  const schema: Joi.ObjectSchema<Object> = Joi.object({
-    postId: Joi.string().required().min(1),
-  });
+  validateHandleAddPostLike(data: object): Joi.ValidationResult {
+    const schema: Joi.ObjectSchema<Object> = Joi.object({
+      postId: Joi.string().required().min(1),
+    });
 
-  return schema.validate(data);
-};
+    return schema.validate(data);
+  }
 
-const validateHandleDeletePostLike = (data: Object): Joi.ValidationResult => {
-  const schema: Joi.ObjectSchema<Object> = Joi.object({
-    postId: Joi.string().required().min(1),
-  });
+  validateHandleDeletePostLike(data: object): Joi.ValidationResult {
+    const schema: Joi.ObjectSchema<Object> = Joi.object({
+      postId: Joi.string().required().min(1),
+    });
 
-  return schema.validate(data);
-};
+    return schema.validate(data);
+  }
 
-const validateHandleAddPostComment = (data: Object): Joi.ValidationResult => {
-  const schema: Joi.ObjectSchema<Object> = Joi.object({
-    postId: Joi.string().required().min(1),
-    comment: Joi.string().required().min(1).max(250),
-  });
+  validateHandleAddPostComment(data: object): Joi.ValidationResult {
+    const schema: Joi.ObjectSchema<Object> = Joi.object({
+      postId: Joi.string().required().min(1),
+      comment: Joi.string().required().min(1).max(250),
+    });
 
-  return schema.validate(data);
-};
+    return schema.validate(data);
+  }
 
-const validateHandleEditPostComment = (data: Object): Joi.ValidationResult => {
-  const schema: Joi.ObjectSchema<Object> = Joi.object({
-    postId: Joi.string().required().min(1),
-    commentId: Joi.string().required().min(1),
-    comment: Joi.string().required().min(1).max(250),
-  });
+  validateHandleEditPostComment(data: object): Joi.ValidationResult {
+    const schema: Joi.ObjectSchema<Object> = Joi.object({
+      postId: Joi.string().required().min(1),
+      commentId: Joi.string().required().min(1),
+      comment: Joi.string().required().min(1).max(250),
+    });
 
-  return schema.validate(data);
-};
+    return schema.validate(data);
+  }
 
-const validateHandleDeletePostComment = (
-  data: Object
-): Joi.ValidationResult => {
-  const schema: Joi.ObjectSchema<Object> = Joi.object({
-    postId: Joi.string().required().min(1),
-    commentId: Joi.string().required().min(1),
-  });
+  validateHandleDeletePostComment(data: object): Joi.ValidationResult {
+    const schema: Joi.ObjectSchema<Object> = Joi.object({
+      postId: Joi.string().required().min(1),
+      commentId: Joi.string().required().min(1),
+    });
 
-  return schema.validate(data);
-};
-
-export {
-  validateHandleNewPost,
-  validateHandleEditPost,
-  validateHandleDeletePost,
-  validateHandleOnePost,
-  validateHandleAddPostLike,
-  validateHandleAddPostComment,
-  validateHandleDeletePostLike,
-  validateHandleEditPostComment,
-  validateHandleDeletePostComment,
-};
+    return schema.validate(data);
+  }
+}
