492104c4de5c0c2d08b38a3d47b2575a64a2ad18
Joao Victor <jov9051@outlook.com>
2023-01-12 21:28:44 -0300

    feat: class ValidateUser criado

--- src/validations/validateUserData.ts
+++ src/validations/validateUserData.ts
@@ -1,47 +1,42 @@
 import Joi from "@hapi/joi";
 
-const validateHandleUserRegister = (data: Object): Joi.ValidationResult => {
-  const schema: Joi.ObjectSchema<Object> = Joi.object({
-    email: Joi.string().required().min(10).max(100),
-    name: Joi.string().required().min(10).max(100),
-    bornAt: Joi.string().required().min(10).max(10),
-    password: Joi.string().required().min(8).max(50),
-  });
+export default class ValidateUser {
+  validateHandleUserRegister(data: object): Joi.ValidationResult {
+    const schema: Joi.ObjectSchema<Object> = Joi.object({
+      email: Joi.string().required().min(10).max(100),
+      name: Joi.string().required().min(10).max(100),
+      bornAt: Joi.string().required().min(10).max(10),
+      password: Joi.string().required().min(8).max(50),
+    });
 
-  return schema.validate(data);
-};
+    return schema.validate(data);
+  }
 
-const validateHandleUserLogin = (data: Object): Joi.ValidationResult => {
-  const schema: Joi.ObjectSchema<Object> = Joi.object({
-    email: Joi.string().required().min(10).max(100),
-    password: Joi.string().required().min(8).max(50),
-  });
+  validateHandleUserLogin(data: object): Joi.ValidationResult {
+    const schema: Joi.ObjectSchema<Object> = Joi.object({
+      email: Joi.string().required().min(10).max(100),
+      password: Joi.string().required().min(8).max(50),
+    });
 
-  return schema.validate(data);
-};
+    return schema.validate(data);
+  }
 
-const validateHandleUserEdit = (data: Object): Joi.ValidationResult => {
-  const schema: Joi.ObjectSchema<Object> = Joi.object({
-    email: Joi.string().required().min(10).max(100),
-    name: Joi.string().required().min(10).max(100),
-    bornAt: Joi.string().required().min(10).max(10),
-    password: Joi.string().required().min(8).max(50),
-  });
+  validateHandleUserEdit(data: object): Joi.ValidationResult {
+    const schema: Joi.ObjectSchema<Object> = Joi.object({
+      email: Joi.string().required().min(10).max(100),
+      name: Joi.string().required().min(10).max(100),
+      bornAt: Joi.string().required().min(10).max(10),
+      password: Joi.string().required().min(8).max(50),
+    });
 
-  return schema.validate(data);
-};
+    return schema.validate(data);
+  }
 
-const validateHandleOneUser = (data: Object): Joi.ValidationResult => {
-  const schema: Joi.ObjectSchema<Object> = Joi.object({
-    email: Joi.string().required().min(10).max(100),
-  });
+  validateHandleOneUser(data: object): Joi.ValidationResult {
+    const schema: Joi.ObjectSchema<Object> = Joi.object({
+      email: Joi.string().required().min(10).max(100),
+    });
 
-  return schema.validate(data);
-};
-
-export {
-  validateHandleUserRegister,
-  validateHandleUserLogin,
-  validateHandleUserEdit,
-  validateHandleOneUser,
-};
+    return schema.validate(data);
+  }
+}
