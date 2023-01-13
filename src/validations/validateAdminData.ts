2772cf9c12d44bd0e5829626da796e38b9aab655
Joao Victor <jov9051@outlook.com>
2023-01-12 21:35:13 -0300

    feat: class ValidateAdmin criado

--- src/validations/validateAdminData.ts
+++ src/validations/validateAdminData.ts
@@ -1,35 +1,31 @@
 import Joi from "@hapi/joi";
 
-const validateHandleAdminEditUser = (data: Object): Joi.ValidationResult => {
-  const schema: Joi.ObjectSchema<Object> = Joi.object({
-    userEmail: Joi.string().required().min(10).max(100),
-    userNewEmail: Joi.string().required().min(10).max(100),
-    userNewName: Joi.string().required().min(10).max(100),
-    userNewBornAt: Joi.date().required(),
-    userNewPassword: Joi.string().required().min(8).max(100),
-  });
+export default class ValidateAdmin {
+  validateHandleAdminEditUser(data: object): Joi.ValidationResult {
+    const schema: Joi.ObjectSchema<Object> = Joi.object({
+      userEmail: Joi.string().required().min(10).max(100),
+      userNewEmail: Joi.string().required().min(10).max(100),
+      userNewName: Joi.string().required().min(10).max(100),
+      userNewBornAt: Joi.date().required(),
+      userNewPassword: Joi.string().required().min(8).max(100),
+    });
 
-  return schema.validate(data);
-};
+    return schema.validate(data);
+  }
 
-const validateHandleAdminDeleteUser = (data: Object): Joi.ValidationResult => {
-  const schema: Joi.ObjectSchema<Object> = Joi.object({
-    userEmail: Joi.string().required().min(10).max(100),
-  });
+  validateHandleAdminDeleteUser(data: object): Joi.ValidationResult {
+    const schema: Joi.ObjectSchema<Object> = Joi.object({
+      userEmail: Joi.string().required().min(10).max(100),
+    });
 
-  return schema.validate(data);
-};
+    return schema.validate(data);
+  }
 
-const validateHandleAdminDeletePost = (data: Object): Joi.ValidationResult => {
-  const schema: Joi.ObjectSchema<Object> = Joi.object({
-    postId: Joi.string().required().min(1),
-  });
+  validateHandleAdminDeletePost(data: object): Joi.ValidationResult {
+    const schema: Joi.ObjectSchema<Object> = Joi.object({
+      postId: Joi.string().required().min(1),
+    });
 
-  return schema.validate(data);
-};
-
-export {
-  validateHandleAdminEditUser,
-  validateHandleAdminDeleteUser,
-  validateHandleAdminDeletePost,
-};
+    return schema.validate(data);
+  }
+}
