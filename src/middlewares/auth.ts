56509c648c0aeab248dc1ee038de09ab65e06c07
Joao Victor <jov9051@outlook.com>
2023-01-12 21:14:04 -0300

    feat: ForbiddenError criado

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -3,26 +3,44 @@ import jwt from "jsonwebtoken";
 import IJwt from "../interfaces/IJson";
 import IReq from "../interfaces/IRequest";
 import { Response, NextFunction } from "express";
+import BadRequestError from "../errors/BadRequestError";
+import InternalError from "../errors/InternalError";
 
-export default function (
-  req: IReq,
-  res: Response,
-  next: NextFunction
-): Response | undefined {
-  const token: string = req.cookies.authorization.split(" ")[1];
+export default class Authorization {
+  execute(req: IReq, res: Response, next: NextFunction): Response | undefined {
+    const token: string = req.cookies.authorization.split(" ")[1];
 
-  try {
-    const verifiedToken: IJwt = jwt.verify(
-      token,
-      process.env.JWT_TOKEN_SECRET as string
-    ) as IJwt;
+    if (token.length === 0) {
+      const error = new BadRequestError("Token não encontrado!");
 
-    req.userId = verifiedToken.id;
+      return res
+        .status(error.statusCode)
+        .json({ error: error.message, status: error.statusCode });
+    }
 
-    req.admin = verifiedToken.admin;
+    try {
+      const verifiedToken: IJwt = jwt.verify(
+        token,
+        process.env.JWT_TOKEN_SECRET as string
+      ) as IJwt;
 
-    next();
-  } catch (err: unknown) {
-    return res.status(500).json({ err });
+      if (!verifiedToken) {
+        const error = new ForbiddenError("Falha na autenticação!");
+        return res
+          .status(error.statusCode)
+          .json({ error: error.message, status: error.statusCode });
+      }
+
+      req.userId = verifiedToken.id;
+
+      req.admin = verifiedToken.admin;
+
+      next();
+    } catch (err: unknown) {
+      const error = new InternalError("Erro Interno");
+      return res
+        .status(error.statusCode)
+        .json({ error: err, status: error.statusCode });
+    }
   }
 }
