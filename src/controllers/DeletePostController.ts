b0a2fca3a3a96b8a73aeec459d8d09104f974b12
Joao Victor <jov9051@outlook.com>
2023-01-14 19:26:15 -0300

    feat: repositories GetPostId e DeletePost adicionados a DeletePostController

--- src/controllers/DeletePostController.ts
+++ src/controllers/DeletePostController.ts
@@ -1,32 +1,37 @@
 import { Response } from "express";
 import IReq from "../interfaces/IRequest";
-import { validateHandleDeletePost } from "../validations/validatePostData";
-import prismaClient from "../database/prismaClient";
-import IDeletePostController from "../interfaces/IDeletePostController";
+import ValidatePost from "../validations/validatePostData";
 import DeletePostService from "../services/DeletePostService";
+import BadRequestError from "../errors/BadRequestError";
+import GetPostIdRepository from "../database/repositories/post/GetPostIdRepository";
+import DeletePostRepository from "../database/repositories/post/DeletePostRepository";
 
-export default class DeletePostController implements IDeletePostController {
+export default class DeletePostController {
   public async handle(req: IReq, res: Response): Promise<Response> {
-    const { error } = validateHandleDeletePost(req.body);
+    const { error } = new ValidatePost().validateHandleDeletePost(req.body);
 
     if (error) {
-      return res.status(400).json({ error });
+      const err = new BadRequestError(error.message);
+
+      return res.status(err.statusCode).json({ error, status: err.statusCode });
     }
 
     const id: string | undefined = req.userId;
 
     const postId: string = req.body.postId;
 
+    const getPostIdRepository: GetPostIdRepository = new GetPostIdRepository();
+
+    const deletePostRepository: DeletePostRepository =
+      new DeletePostRepository();
+
     const deletePostService: DeletePostService = new DeletePostService(
-      prismaClient
+      getPostIdRepository,
+      deletePostRepository
     );
 
     try {
-      // eslint-disable-next-line no-unused-vars
-      const deletedPost: Object = await deletePostService.execute(
-        Number(id),
-        Number(postId)
-      );
+      await deletePostService.execute(Number(id), Number(postId));
 
       return res.status(204).send();
     } catch (err: any) {
