4fa3058df7165091575c2e049323f6800e08815e
Joao Victor <jov9051@outlook.com>
2023-01-15 16:42:45 -0300

    feat: repositories adicionados a AddPostLikeService

--- src/services/AddPostLikeService.ts
+++ src/services/AddPostLikeService.ts
@@ -2,23 +2,30 @@ import BadRequestError from "../errors/BadRequestError";
 import { Likes, Post, User } from "@prisma/client";
 import IGetPostIdRepository from "../interfaces/IGetPostIdRepository";
 import IGetUserIdRepository from "../interfaces/IGetUserIdRepository";
+import IGetLikeIdRepository from "../interfaces/IGetLikeIdRepository";
+import ICreateLikeRepository from "../interfaces/ICreateLikeRepository";
 
 export default class AddPostLikeService {
   private readonly getPostIdRepository: IGetPostIdRepository;
   private readonly getUserIdRepository: IGetUserIdRepository;
+  private readonly getLikeIdRepository: IGetLikeIdRepository;
+  private readonly createLikeRepository: ICreateLikeRepository;
 
   constructor(
     getPostIdRepository: IGetPostIdRepository,
-    getUserIdRepository: IGetUserIdRepository
+    getUserIdRepository: IGetUserIdRepository,
+    getLikeIdRepository: IGetLikeIdRepository,
+    createLikeRepository: ICreateLikeRepository
   ) {
     this.getPostIdRepository = getPostIdRepository;
     this.getUserIdRepository = getUserIdRepository;
-    this.repository = repository;
+    this.getLikeIdRepository = getLikeIdRepository;
+    this.createLikeRepository = createLikeRepository;
   }
 
-  public async execute(postId: number, id: number | undefined): Promise<Likes> {
+  public async execute(postId: number, id: number): Promise<Likes> {
     const isPostRegistered: Post | null =
-      await this.getPostIdRepository.execute(postId);
+      await this.getPostIdRepository.execute(id, postId);
 
     if (isPostRegistered === null) {
       throw new BadRequestError("Post não encontrado!");
@@ -29,26 +36,17 @@ export default class AddPostLikeService {
     if (user === null) throw new BadRequestError("User não encontrado!");
 
     const isLikeRegistered: Likes | null =
-      await this.repository.likes.findFirst({
-        where: {
-          postId,
-          AND: {
-            userId: id,
-          },
-        },
-      });
+      await this.getLikeIdRepository.execute(id, postId);
 
     if (isLikeRegistered !== null) {
       throw new BadRequestError("Like já registrado!");
     }
 
-    const addedLike = await this.repository.likes.create({
-      data: {
-        postId,
-        likedBy: user!.email,
-        userId: user!.id,
-      },
-    });
+    const addedLike = await this.createLikeRepository.execute(
+      isPostRegistered.id,
+      user.email,
+      user.id
+    );
 
     return addedLike;
   }
