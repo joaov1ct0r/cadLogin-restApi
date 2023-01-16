621c5978ba538a575fad45181e571136fa40f5c6
Joao Victor <jov9051@outlook.com>
2023-01-16 16:55:40 -0300

    feat: repositories adicionados a DeletePostLikeController

--- src/controllers/DeletePostLikeController.ts
+++ src/controllers/DeletePostLikeController.ts
@@ -1,33 +1,41 @@
 import IReq from "../interfaces/IRequest";
 import { Response } from "express";
-import { validateHandleDeletePostLike } from "../validations/validatePostData";
+import ValidatePost from "../validations/validatePostData";
 import DeletePostLikeService from "../services/DeletePostLikeService";
-import IDeletePostLikeController from "../interfaces/IDeletePostLikeController";
-import prismaClient from "../database/prismaClient";
+import BadRequestError from "../errors/BadRequestError";
+import GetPostIdRepository from "../database/repositories/post/GetPostIdRepository";
+import GetLikeIdRepository from "../database/repositories/likes/GetLikeIdRepository";
+import DeleteLikeRepository from "../database/repositories/likes/DeleteLikeRepository";
 
-export default class DeletePostLikeController
-  implements IDeletePostLikeController
-{
+export default class DeletePostLikeController {
   public async handle(req: IReq, res: Response): Promise<Response> {
-    const { error } = validateHandleDeletePostLike(req.body);
+    const { error } = new ValidatePost().validateHandleDeletePostLike(req.body);
 
     if (error) {
-      return res.status(400).json({ error });
+      const err = new BadRequestError(error.message);
+      return res.status(err.statusCode).json({ error, status: err.statusCode });
     }
 
     const userId: string | undefined = req.userId;
 
     const postId: string = req.body.postId;
 
+    const getPostIdRepository: GetPostIdRepository = new GetPostIdRepository();
+
+    const getLikeIdRepository: GetLikeIdRepository = new GetLikeIdRepository();
+
+    const deleteLikeRepository: DeleteLikeRepository =
+      new DeleteLikeRepository();
+
     const deletePostLikeService: DeletePostLikeService =
-      new DeletePostLikeService(prismaClient);
+      new DeletePostLikeService(
+        getPostIdRepository,
+        getLikeIdRepository,
+        deleteLikeRepository
+      );
 
     try {
-      // eslint-disable-next-line no-unused-vars
-      const deletedLike: Object = await deletePostLikeService.execute(
-        Number(userId),
-        Number(postId)
-      );
+      await deletePostLikeService.execute(Number(userId), Number(postId));
 
       return res.status(204).send();
     } catch (err: any) {
