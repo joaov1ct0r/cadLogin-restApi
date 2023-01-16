e419f4a8231dbbab1346f4f30f5024431e3a218f
Joao Victor <jov9051@outlook.com>
2023-01-16 16:49:39 -0300

    feat: repositories adicionados a DeletePostLikeService

--- src/services/DeletePostLikeService.ts
+++ src/services/DeletePostLikeService.ts
@@ -1,51 +1,39 @@
 import BadRequestError from "../errors/BadRequestError";
-import IDeletePostLikeService from "../interfaces/IDeletePostLikeService";
-import { Likes, Post, PrismaClient } from "@prisma/client";
+import { Likes, Post } from "@prisma/client";
+import IGetPostIdRepository from "../interfaces/IGetPostIdRepository";
+import IGetLikeIdRepository from "../interfaces/IGetLikeIdRepository";
+import IDeleteLikeRepository from "../interfaces/IDeleteLikeRepository";
 
-export default class DeletePostLikeService implements IDeletePostLikeService {
-  private readonly repository: PrismaClient;
+export default class DeletePostLikeService {
+  private readonly getPostIdRepository: IGetPostIdRepository;
+  private readonly getLikeIdRepository: IGetLikeIdRepository;
+  private readonly deleteLikeRepository: IDeleteLikeRepository;
 
-  constructor(repository: PrismaClient) {
-    this.repository = repository;
+  constructor(
+    getPostIdRepository: IGetPostIdRepository,
+    getLikeIdRepository: IGetLikeIdRepository,
+    deleteLikeRepository: IDeleteLikeRepository
+  ) {
+    this.getPostIdRepository = getPostIdRepository;
+    this.getLikeIdRepository = getLikeIdRepository;
+    this.deleteLikeRepository = deleteLikeRepository;
   }
 
-  public async execute(
-    userId: number | undefined,
-    postId: number
-  ): Promise<Object> {
-    const isPostRegistered: Post | null = await this.repository.post.findUnique(
-      {
-        where: { id: postId },
-      }
-    );
+  public async execute(userId: number, postId: number): Promise<void> {
+    const isPostRegistered: Post | null =
+      await this.getPostIdRepository.execute(undefined, postId);
 
     if (isPostRegistered === null) {
       throw new BadRequestError("Post não encontrado!");
     }
 
     const isLikeRegistered: Likes | null =
-      await this.repository.likes.findFirst({
-        where: {
-          userId,
-          AND: {
-            postId,
-          },
-        },
-      });
+      await this.getLikeIdRepository.execute(userId, postId);
 
     if (isLikeRegistered === null) {
       throw new BadRequestError("Like não encontrado!");
     }
 
-    await this.repository.likes.deleteMany({
-      where: {
-        userId,
-        AND: {
-          postId,
-        },
-      },
-    });
-
-    return { message: "Deleted" };
+    await this.deleteLikeRepository.execute(userId, postId);
   }
 }
