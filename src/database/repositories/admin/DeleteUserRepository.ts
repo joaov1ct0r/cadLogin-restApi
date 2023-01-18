b7a260f8bdc302abe6962eea38b41a364e61d5ea
Joao Victor <jov9051@outlook.com>
2023-01-18 19:34:58 -0300

    feat: repository DeleteUser criado

--- src/database/repositories/admin/DeleteUserRepository.ts
+++ src/database/repositories/admin/DeleteUserRepository.ts
@@ -1,11 +1,33 @@
-interface IAdminDeleteUserRepository {
-  execute(email: string): Promise<void>;
-}
+import { PrismaClient, User } from "@prisma/client";
+import prismaClient from "../../prismaClient";
+import IAdminDeleteUserRepository from "../../../interfaces/IAdminDeleteUserRepository";
 
 export default class AdminDeleteUserRepository
   implements IAdminDeleteUserRepository
 {
-  execute(email: string): Promise<void> {
-    throw new Error("Method not implemented.");
+  private readonly repository: PrismaClient;
+
+  constructor() {
+    this.repository = prismaClient;
+  }
+
+  async execute(user: User): Promise<void> {
+    await this.repository.user.delete({
+      where: {
+        email: user.email,
+      },
+    });
+
+    await this.repository.post.deleteMany({
+      where: { userId: user.id },
+    });
+
+    await this.repository.likes.deleteMany({
+      where: { userId: user.id },
+    });
+
+    await this.repository.comment.deleteMany({
+      where: { userId: user.id },
+    });
   }
 }
