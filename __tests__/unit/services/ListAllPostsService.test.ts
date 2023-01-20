e4ed0b77127187545f561cfea51b67fde12fe94c
Joao Victor <jov9051@outlook.com>
2023-01-20 18:17:21 -0300

    test: ListAllPostsService refatorado

--- __tests__/unit/services/ListAllPostsService.test.ts
+++ __tests__/unit/services/ListAllPostsService.test.ts
@@ -1,21 +1,23 @@
-import { mockDeep } from "jest-mock-extended";
+import { mock } from "jest-mock-extended";
 import ListAllPostsService from "../../../src/services/ListAllPostsService";
-import { PrismaClient } from "@prisma/client";
+import IListAllPostsRepository from "../../../src/interfaces/IListAllPostsRepository";
 
 const makeSut = () => {
-  const prismaSpyRepository = mockDeep<PrismaClient>();
+  const listAllPostsRepository = mock<IListAllPostsRepository>();
 
-  const sut: ListAllPostsService = new ListAllPostsService(prismaSpyRepository);
+  const sut: ListAllPostsService = new ListAllPostsService(
+    listAllPostsRepository
+  );
 
-  return { sut, prismaSpyRepository };
+  return { sut, listAllPostsRepository };
 };
 
 describe("list all posts service", () => {
   describe("when execute is called", () => {
     it("should return all posts", async () => {
-      const { sut, prismaSpyRepository } = makeSut();
+      const { sut, listAllPostsRepository } = makeSut();
 
-      prismaSpyRepository.post.findMany.mockResolvedValueOnce([
+      listAllPostsRepository.execute.mockResolvedValueOnce([
         {
           id: 1,
           author: "any@mail.com.br",
