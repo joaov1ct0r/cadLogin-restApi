ff760067a820e55b55e24abff654f13c0f1aa80c
Joao Victor <jov9051@outlook.com>
2023-01-19 21:34:17 -0300

    test: EditUserService refatorado

--- __tests__/unit/services/EditUserService.test.ts
+++ __tests__/unit/services/EditUserService.test.ts
@@ -1,49 +1,31 @@
-import { mockDeep } from "jest-mock-extended";
+import { mock } from "jest-mock-extended";
 import EditUserService from "../../../src/services/EditUserService";
-import { PrismaClient } from "@prisma/client";
+import IEditUserRepository from "../../../src/interfaces/IEditUserRepository";
 
 const makeSut = () => {
-  const prismaSpyRepository = mockDeep<PrismaClient>();
+  const editUserRepository = mock<IEditUserRepository>();
 
-  const sut: EditUserService = new EditUserService(prismaSpyRepository);
+  const sut: EditUserService = new EditUserService(editUserRepository);
 
-  return { prismaSpyRepository, sut };
+  return { editUserRepository, sut };
 };
 
 describe("edit user service", () => {
   describe("when execute is called", () => {
-    it("should return number of affected lines if succeed to edit user", async () => {
-      const { sut, prismaSpyRepository } = makeSut();
+    it("should edit user", async () => {
+      const { sut, editUserRepository } = makeSut();
 
-      const userData = {
-        email: "user@mail.com.br",
-        password: "123123123",
-        name: "user name",
-        bornAt: "01/09/2001",
-        userId: 1,
-      };
+      editUserRepository.execute.mockResolvedValueOnce();
 
-      prismaSpyRepository.user.update.mockResolvedValueOnce({
-        id: 1,
-        email: "user@mail.com.br",
-        password: "123123123",
-        name: "user name",
-        bornAt: "01/09/2001",
-        admin: false,
-      });
+      await sut.execute(
+        "user@mail.com.br",
+        "123123123",
+        "user name",
+        "01/09/2001",
+        1
+      );
 
-      const editedLines = await sut.execute(userData);
-
-      expect(editedLines).toEqual({
-        id: 1,
-        email: "user@mail.com.br",
-        password: "123123123",
-        name: "user name",
-        bornAt: "01/09/2001",
-        admin: false,
-      });
-
-      expect(editedLines).toHaveProperty("id");
+      expect(editUserRepository.execute).toHaveBeenCalledTimes(1);
     });
   });
 });
