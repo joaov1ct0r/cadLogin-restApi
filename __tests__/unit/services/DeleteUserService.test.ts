7cb2b36cd79f5ccee24c79ef8951c5c27d00ded5
Joao Victor <jov9051@outlook.com>
2023-01-19 21:37:28 -0300

    test: DeleteUserService refatorado

--- __tests__/unit/services/DeleteUserService.test.ts
+++ __tests__/unit/services/DeleteUserService.test.ts
@@ -1,46 +1,25 @@
-import { Prisma, PrismaClient } from "@prisma/client";
-import { mockDeep } from "jest-mock-extended";
+import { mock } from "jest-mock-extended";
 import DeleteUserService from "../../../src/services/DeleteUserService";
+import IDeleteUserRepository from "../../../src/interfaces/IDeleteUserRepository";
 
 const makeSut = () => {
-  const prismaSpyRepository = mockDeep<PrismaClient>();
+  const deleteUserRepository = mock<IDeleteUserRepository>();
 
-  const sut: DeleteUserService = new DeleteUserService(prismaSpyRepository);
+  const sut: DeleteUserService = new DeleteUserService(deleteUserRepository);
 
-  return { prismaSpyRepository, sut };
+  return { deleteUserRepository, sut };
 };
 
 describe("delete user service", () => {
   describe("when execute is called", () => {
     it("should return object when succed to delete user", async () => {
-      const { sut, prismaSpyRepository } = makeSut();
+      const { sut, deleteUserRepository } = makeSut();
 
-      prismaSpyRepository.user.delete.mockResolvedValueOnce({
-        id: 1,
-        email: "user@mail.com.br",
-        password: "123123123",
-        name: "user name",
-        bornAt: "01/09/2001",
-        admin: false,
-      });
+      deleteUserRepository.execute.mockResolvedValueOnce();
 
-      prismaSpyRepository.post.deleteMany.mockResolvedValueOnce(
-        {} as Prisma.BatchPayload
-      );
+      await sut.execute(1);
 
-      prismaSpyRepository.likes.deleteMany.mockResolvedValueOnce(
-        {} as Prisma.BatchPayload
-      );
-
-      prismaSpyRepository.comment.deleteMany.mockResolvedValueOnce(
-        {} as Prisma.BatchPayload
-      );
-
-      const deletedLines = await sut.execute(1);
-
-      expect(deletedLines).toHaveProperty("message");
-
-      expect(deletedLines).toEqual({ message: "Deletado" });
+      expect(deleteUserRepository.execute).toHaveBeenCalledTimes(1);
     });
   });
 });
