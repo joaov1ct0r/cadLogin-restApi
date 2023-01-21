894fc93f0a37b05130350c46ba648c5379e743fc
Joao Victor <jov9051@outlook.com>
2023-01-20 21:27:57 -0300

    test: DeletePostCommentService refatorado

--- __tests__/unit/services/DeletePostCommentService.test.ts
+++ __tests__/unit/services/DeletePostCommentService.test.ts
@@ -1,24 +1,37 @@
-import { Prisma, PrismaClient } from "@prisma/client";
-import { mockDeep } from "jest-mock-extended";
+import { mock } from "jest-mock-extended";
 import BadRequestError from "../../../src/errors/BadRequestError";
 import DeletePostCommentService from "../../../src/services/DeletePostCommentService";
+import IGetPostIdRepository from "../../../src/interfaces/IGetPostIdRepository";
+import IGetCommentRepository from "../../../src/interfaces/IGetCommentRepository";
+import IDeleteCommentRepository from "../../../src/interfaces/IDeleteCommentRepository";
 
 const makeSut = () => {
-  const prismaSpyRepository = mockDeep<PrismaClient>();
+  const getPostIdRepository = mock<IGetPostIdRepository>();
+
+  const getCommentIdRepository = mock<IGetCommentRepository>();
+
+  const deleteCommentRepository = mock<IDeleteCommentRepository>();
 
   const sut: DeletePostCommentService = new DeletePostCommentService(
-    prismaSpyRepository
+    getPostIdRepository,
+    getCommentIdRepository,
+    deleteCommentRepository
   );
 
-  return { sut, prismaSpyRepository };
+  return {
+    sut,
+    getPostIdRepository,
+    getCommentIdRepository,
+    deleteCommentRepository,
+  };
 };
 
 describe("delete post comment service", () => {
   describe("when execute is called", () => {
     it("should throw an exception if post is null", async () => {
-      const { sut, prismaSpyRepository } = makeSut();
+      const { sut, getPostIdRepository } = makeSut();
 
-      prismaSpyRepository.post.findUnique.mockResolvedValueOnce(null);
+      getPostIdRepository.execute.mockResolvedValueOnce(null);
 
       expect(async () => {
         await sut.execute(1, 1, 1);
@@ -26,33 +39,38 @@ describe("delete post comment service", () => {
     });
 
     it("should throw an exception if comment is null", async () => {
-      const { sut, prismaSpyRepository } = makeSut();
+      const { sut, getPostIdRepository, getCommentIdRepository } = makeSut();
 
-      prismaSpyRepository.post.findUnique.mockResolvedValueOnce({
+      getPostIdRepository.execute.mockResolvedValueOnce({
         id: 1,
         author: "any@mail.com.br",
         content: "titulo de post",
         userId: 1,
       });
 
-      prismaSpyRepository.comment.findFirst.mockResolvedValueOnce(null);
+      getCommentIdRepository.execute.mockResolvedValueOnce(null);
 
       expect(async () => {
         await sut.execute(1, 1, 1);
       }).rejects.toThrow(new BadRequestError("Comentario não encontrado!"));
     });
 
-    it("should return number of deleted lines of deleted comment", async () => {
-      const { sut, prismaSpyRepository } = makeSut();
+    it("should delete comment", async () => {
+      const {
+        sut,
+        getPostIdRepository,
+        getCommentIdRepository,
+        deleteCommentRepository,
+      } = makeSut();
 
-      prismaSpyRepository.post.findUnique.mockResolvedValueOnce({
+      getPostIdRepository.execute.mockResolvedValueOnce({
         id: 1,
         author: "any@mail.com.br",
         content: "titulo de post",
         userId: 1,
       });
 
-      prismaSpyRepository.comment.findFirst.mockResolvedValueOnce({
+      getCommentIdRepository.execute.mockResolvedValueOnce({
         postId: 1,
         id: 1,
         author: "any@mail.com.br",
@@ -60,15 +78,15 @@ describe("delete post comment service", () => {
         userId: 1,
       });
 
-      prismaSpyRepository.comment.deleteMany.mockResolvedValueOnce(
-        {} as Prisma.BatchPayload
-      );
+      deleteCommentRepository.execute.mockResolvedValueOnce();
 
-      const deletedLines = await sut.execute(1, 1, 1);
+      await sut.execute(1, 1, 1);
 
-      expect(deletedLines).toHaveProperty("message");
+      expect(getPostIdRepository.execute).toHaveBeenCalledTimes(1);
 
-      expect(deletedLines).toEqual({ message: "Comment deletado!" });
+      expect(getCommentIdRepository.execute).toHaveBeenCalledTimes(1);
+
+      expect(deleteCommentRepository.execute).toHaveBeenCalledTimes(1);
     });
   });
 });
