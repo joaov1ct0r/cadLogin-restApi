d8ba730101687b2ee796354026f704ea1ddb401b
Joao Victor <jov9051@outlook.com>
2023-01-20 21:01:28 -0300

    test: DeletePostLikeService refatorado

--- __tests__/unit/services/DeletePostLikeService.test.ts
+++ __tests__/unit/services/DeletePostLikeService.test.ts
@@ -1,24 +1,37 @@
-import { mockDeep } from "jest-mock-extended";
+import { mock } from "jest-mock-extended";
 import BadRequestError from "../../../src/errors/BadRequestError";
-import { Prisma, PrismaClient } from "@prisma/client";
 import DeletePostLikeService from "../../../src/services/DeletePostLikeService";
+import IGetPostIdRepository from "../../../src/interfaces/IGetPostIdRepository";
+import IGetLikeIdRepository from "../../../src/interfaces/IGetLikeIdRepository";
+import IDeleteLikeRepository from "../../../src/interfaces/IDeleteLikeRepository";
 
 const makeSut = () => {
-  const prismaSpyRepository = mockDeep<PrismaClient>();
+  const getPostIdRepository = mock<IGetPostIdRepository>();
+
+  const getLikeIdRepository = mock<IGetLikeIdRepository>();
+
+  const deleteLikeRepository = mock<IDeleteLikeRepository>();
 
   const sut: DeletePostLikeService = new DeletePostLikeService(
-    prismaSpyRepository
+    getPostIdRepository,
+    getLikeIdRepository,
+    deleteLikeRepository
   );
 
-  return { sut, prismaSpyRepository };
+  return {
+    sut,
+    getPostIdRepository,
+    getLikeIdRepository,
+    deleteLikeRepository,
+  };
 };
 
 describe("delete post like service", () => {
   describe("when execute is called", () => {
     it("should throw an exception if post is null", async () => {
-      const { sut, prismaSpyRepository } = makeSut();
+      const { sut, getPostIdRepository } = makeSut();
 
-      prismaSpyRepository.post.findUnique.mockResolvedValueOnce(null);
+      getPostIdRepository.execute.mockResolvedValueOnce(null);
 
       expect(async () => {
         await sut.execute(1, 1);
@@ -26,48 +39,53 @@ describe("delete post like service", () => {
     });
 
     it("should throw an exception if like is null", async () => {
-      const { sut, prismaSpyRepository } = makeSut();
+      const { sut, getPostIdRepository, getLikeIdRepository } = makeSut();
 
-      prismaSpyRepository.post.findUnique.mockResolvedValueOnce({
+      getPostIdRepository.execute.mockResolvedValueOnce({
         id: 1,
         author: "any@mail.com.br",
         content: "titulo de post",
         userId: 1,
       });
 
-      prismaSpyRepository.likes.findFirst.mockResolvedValueOnce(null);
+      getLikeIdRepository.execute.mockResolvedValueOnce(null);
 
       expect(async () => {
         await sut.execute(1, 1);
       }).rejects.toThrow(new BadRequestError("Like não encontrado!"));
     });
 
-    it("should return number of deleted lines when deleted like", async () => {
-      const { sut, prismaSpyRepository } = makeSut();
+    it("should delete like", async () => {
+      const {
+        sut,
+        getPostIdRepository,
+        getLikeIdRepository,
+        deleteLikeRepository,
+      } = makeSut();
 
-      prismaSpyRepository.post.findUnique.mockResolvedValueOnce({
+      getPostIdRepository.execute.mockResolvedValueOnce({
         id: 1,
         author: "any@mail.com.br",
         content: "titulo de post",
         userId: 1,
       });
 
-      prismaSpyRepository.likes.findFirst.mockResolvedValueOnce({
+      getLikeIdRepository.execute.mockResolvedValueOnce({
         postId: 1,
         userId: 1,
         id: 1,
         likedBy: "any@mail.com.br",
       });
 
-      prismaSpyRepository.likes.deleteMany.mockResolvedValueOnce(
-        {} as Prisma.BatchPayload
-      );
+      deleteLikeRepository.execute.mockResolvedValueOnce();
 
-      const deletedLines = await sut.execute(1, 1);
+      await sut.execute(1, 1);
 
-      expect(deletedLines).toHaveProperty("message");
+      expect(getPostIdRepository.execute).toHaveBeenCalledTimes(1);
 
-      expect(deletedLines).toEqual({ message: "Deleted" });
+      expect(getLikeIdRepository.execute).toHaveBeenCalledTimes(1);
+
+      expect(deleteLikeRepository.execute).toHaveBeenCalledTimes(1);
     });
   });
 });
