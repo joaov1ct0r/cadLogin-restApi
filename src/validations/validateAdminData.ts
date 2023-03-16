956653c2b96ea70c54bc5963761351bcafda0ed0
joaov1ct0r <jov9051@outlook.com>
2023-03-16 19:12:17 -0300

    refactor: validateAdminData

--- src/validations/validateAdminData.ts
+++ src/validations/validateAdminData.ts
@@ -1,8 +1,10 @@
 import Joi from "@hapi/joi";
 
 export default class ValidateAdmin {
+  private schema: Joi.ObjectSchema<Object> | undefined;
+
   validateHandleAdminEditUser(data: object): Joi.ValidationResult {
-    const schema: Joi.ObjectSchema<Object> = Joi.object({
+    this.schema = Joi.object({
       userEmail: Joi.string().required().min(10).max(100),
       userNewEmail: Joi.string().required().min(10).max(100),
       userNewName: Joi.string().required().min(10).max(100),
@@ -10,22 +12,22 @@ export default class ValidateAdmin {
       userNewPassword: Joi.string().required().min(8).max(100),
     });
 
-    return schema.validate(data);
+    return this.schema.validate(data);
   }
 
   validateHandleAdminDeleteUser(data: object): Joi.ValidationResult {
-    const schema: Joi.ObjectSchema<Object> = Joi.object({
+    this.schema = Joi.object({
       userEmail: Joi.string().required().min(10).max(100),
     });
 
-    return schema.validate(data);
+    return this.schema.validate(data);
   }
 
   validateHandleAdminDeletePost(data: object): Joi.ValidationResult {
-    const schema: Joi.ObjectSchema<Object> = Joi.object({
+    this.schema = Joi.object({
       postId: Joi.string().required().min(1),
     });
 
-    return schema.validate(data);
+    return this.schema.validate(data);
   }
 }
