6098219f69a4a7bbe8bf4dad2625736e0b7b29c3
joaov1ct0r <jov9051@outlook.com>
2023-03-24 15:16:29 -0300

    refactor: EditUserController

--- src/controllers/EditUserController.ts
+++ src/controllers/EditUserController.ts
@@ -6,8 +6,18 @@ import BadRequestError from "../errors/BadRequestError";
 import EditUserRepository from "../database/repositories/user/EditUserRepository";
 
 export default class EditUserController {
+  private readonly validateUser: ValidateUser;
+  private readonly editUserRepository: EditUserRepository;
+  private readonly editUserService: EditUserService;
+
+  constructor() {
+    this.validateUser = new ValidateUser();
+    this.editUserRepository = new EditUserRepository();
+    this.editUserService = new EditUserService(this.editUserRepository);
+  }
+
   public async handle(req: IReq, res: Response): Promise<Response> {
-    const { error } = new ValidateUser().validateHandleUserEdit(req.body);
+    const { error } = this.validateUser.validateHandleUserEdit(req.body);
 
     if (error) {
       const err = new BadRequestError(error.message);
@@ -25,14 +35,14 @@ export default class EditUserController {
 
     const id: string | undefined = req.userId;
 
-    const editUserRepository: EditUserRepository = new EditUserRepository();
-
-    const editUserService: EditUserService = new EditUserService(
-      editUserRepository
-    );
-
     try {
-      await editUserService.execute(email, password, name, bornAt, Number(id));
+      await this.editUserService.execute(
+        email,
+        password,
+        name,
+        bornAt,
+        Number(id)
+      );
 
       return res.status(204).send();
     } catch (err: any) {
