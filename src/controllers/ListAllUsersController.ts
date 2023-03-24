38229104d450cee1c1a688dbc65810b4f9f5d8ed
joaov1ct0r <jov9051@outlook.com>
2023-03-24 15:18:36 -0300

    refactor: ListAllUsersController

--- src/controllers/ListAllUsersController.ts
+++ src/controllers/ListAllUsersController.ts
@@ -4,14 +4,19 @@ import { User } from "@prisma/client";
 import ListUsersRepository from "../database/repositories/user/ListUsersRepository";
 
 export default class ListAllUsersController {
-  public async handle(req: Request, res: Response): Promise<Response> {
-    const listUsersRepository: ListUsersRepository = new ListUsersRepository();
-    const listAllUsersService: ListAllUsersService = new ListAllUsersService(
-      listUsersRepository
-    );
+  private readonly listUsersRepository: ListUsersRepository;
+  private readonly listAllUsersService: ListAllUsersService;
 
+  constructor() {
+    this.listUsersRepository = new ListUsersRepository();
+    this.listAllUsersService = new ListAllUsersService(
+      this.listUsersRepository
+    );
+  }
+
+  public async handle(req: Request, res: Response): Promise<Response> {
     try {
-      const users: User[] = await listAllUsersService.execute();
+      const users: User[] = await this.listAllUsersService.execute();
 
       return res.status(200).json({ users, status: 200 });
     } catch (err: any) {
