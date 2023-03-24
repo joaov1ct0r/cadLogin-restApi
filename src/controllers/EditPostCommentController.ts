b542d6b693bc8c15b96af5a5a1d8e6725284d9fe
joaov1ct0r <jov9051@outlook.com>
2023-03-23 21:08:41 -0300

    refactor: EditPostCommentController

--- src/controllers/EditPostCommentController.ts
+++ src/controllers/EditPostCommentController.ts
@@ -9,10 +9,29 @@ import GetUserIdRepository from "../database/repositories/user/GetUserIdReposito
 import UpdateCommentRepository from "../database/repositories/comment/UpdateCommentRepository";
 
 export default class EditPostCommentController {
-  public async handle(req: IReq, res: Response): Promise<Response> {
-    const { error } = new ValidatePost().validateHandleEditPostComment(
-      req.body
+  private readonly validatePost: ValidatePost;
+  private readonly getPostIdRepository: GetPostIdRepository;
+  private readonly getCommentRepository: GetCommentRepository;
+  private readonly getUserIdRepository: GetUserIdRepository;
+  private readonly updateCommentRepository: UpdateCommentRepository;
+  private readonly editPostCommentService: EditPostCommentService;
+
+  constructor() {
+    this.validatePost = new ValidatePost();
+    this.getPostIdRepository = new GetPostIdRepository();
+    this.getCommentRepository = new GetCommentRepository();
+    this.getUserIdRepository = new GetUserIdRepository();
+    this.updateCommentRepository = new UpdateCommentRepository();
+    this.editPostCommentService = new EditPostCommentService(
+      this.getPostIdRepository,
+      this.getCommentRepository,
+      this.getUserIdRepository,
+      this.updateCommentRepository
     );
+  }
+
+  public async handle(req: IReq, res: Response): Promise<Response> {
+    const { error } = this.validatePost.validateHandleEditPostComment(req.body);
 
     if (error) {
       const err = new BadRequestError(error.message);
@@ -27,26 +46,8 @@ export default class EditPostCommentController {
 
     const userId: string | undefined = req.userId;
 
-    const getPostIdRepository: GetPostIdRepository = new GetPostIdRepository();
-
-    const getCommentRepository: GetCommentRepository =
-      new GetCommentRepository();
-
-    const getUserIdRepository: GetUserIdRepository = new GetUserIdRepository();
-
-    const updateCommentRepository: UpdateCommentRepository =
-      new UpdateCommentRepository();
-
-    const editPostCommentService: EditPostCommentService =
-      new EditPostCommentService(
-        getPostIdRepository,
-        getCommentRepository,
-        getUserIdRepository,
-        updateCommentRepository
-      );
-
     try {
-      await editPostCommentService.execute(
+      await this.editPostCommentService.execute(
         Number(userId),
         Number(postId),
         Number(commentId),
